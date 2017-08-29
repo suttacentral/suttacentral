25d379dd0d793a37c89781b5365ff52db18f7119
Jakub Semik <jakub.semik@stxnext.pl>
2017-08-29 09:12:35 +0200

    Adjusted existing test.

--- server/server/suttas/tests/test_app.py
+++ server/server/suttas/tests/test_app.py
@@ -2,5 +2,5 @@ from app import api, HelloWorld
 
 
 def test_hello(client):
-    res = client.get(api.url_for(HelloWorld))
+    res = client.get(api.url_for(HelloWorld, name='test'))
     assert res.status_code == 200
