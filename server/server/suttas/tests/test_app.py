94ad8c4639f5ac6a51b996a8e0c1451274ab8b7f
Jakub Semik <jakub.semik@stxnext.pl>
2017-08-29 09:16:49 +0200

    Added docstring to the test

--- server/server/suttas/tests/test_app.py
+++ server/server/suttas/tests/test_app.py
@@ -2,5 +2,8 @@ from app import api, HelloWorld
 
 
 def test_hello(client):
+    """
+    It is just a sample test so that i can set up test env.
+    """
     res = client.get(api.url_for(HelloWorld, name='test'))
     assert res.status_code == 200
