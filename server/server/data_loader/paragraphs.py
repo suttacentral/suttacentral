6c857e94a0eda61e4e1970d8f4e0861ec437a743
Hubert Dworczyński <hubert.dworczynski@gmail.com>
2017-10-18 13:43:19 +0200

    [SUCE-244]: Sutta View: textual information (#92)

---
+++ server/server/data_loader/paragraphs.py
@@ -0,0 +1,12 @@
+import json
+from pathlib import Path
+
+
+def load_paragraphs(db, additional_info_dir: Path):
+    print('Loading paragraphs')
+
+    paragraphs_collection = db['paragraphs']
+    with open(f'{additional_info_dir}/paragraphs.json') as f:
+        data = json.load(f)
+
+    paragraphs_collection.import_bulk(data)
