fd93d09562d310acc893a6efd226f6dfedfeb4d0
Blake Walsh <blakeofthepath@gmail.com>
2017-12-22 15:19:14 +0200

    Utilize change tracker

--- server/server/data_loader/dictionary_full.py
+++ server/server/data_loader/dictionary_full.py
@@ -1,16 +1,19 @@
 import json
 from tqdm import tqdm
+from collections import Counter
 
-def load_dictionary_full(db, dictionaries_dir):
+def load_dictionary_full(db, dictionaries_dir, change_tracker):
     print('Loading dictionary_full')
-    dictionary_full = (dictionaries_dir / 'en').glob('*.json')
+    
+    dictionary_full_files = list((dictionaries_dir / 'en').glob('*.json'))
+    if change_tracker.is_any_file_new_or_changed(dictionary_full_files)
     dictionary_full_collection = db['dictionary_full']
-    db['dictionary_full'].truncate()
     docs = []
-    for dictionary in tqdm(dictionary_full):
+    for dictionary in tqdm(dictionary_full_files):
         with dictionary.open('r', encoding='utf-8') as f:
-
-            for entry in json.load(f):
+            
+            for entry in json.load(f): 
                 docs.append({'dictname': dictionary.stem, **entry})
-
+    
+    dictionary_full_collection.truncate()
     dictionary_full_collection.import_bulk(docs)
