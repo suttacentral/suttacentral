824ea5a9edcbc50cd84919fe1e2614f89b43d9c8
Blake Walsh <blakeofthepath@gmail.com>
2018-01-02 18:19:06 +0200

    Added the actually properly updated version of textfunctions (not that the old version wasn't basically correct, just mega ugly) Also some simple sanity tests for the functions

--- server/server/data_loader/textfunctions.py
+++ server/server/data_loader/textfunctions.py
@@ -1,38 +1,44 @@
 import regex
 import unicodedata
 
-def pali_sort_key(input, _charvalue = {}):
-    "for sorting strings into pali alphabetical order"
-    if len(_charvalue) == 0:
-        charInorder = [
-            '#', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'ā',
-            'i', 'ī', 'u', 'ū', 'e', 'o', 'ṁ', 'ṃ', 'k', 'kh', 'g', 'gh', 'ṅ', 'c',
-            'ch', 'j', 'jh', 'ñ', 'ṭ', 'ṭh', 'ḍ', 'ḍh', 'ṇ', 't', 'th', 'd',
-            'dh', 'n', 'p', 'ph', 'b', 'bh', 'm', 'y', 'r', 'l', 'ḷ', 'v', 's', 'h',
-        ]
-        for i in range(0, len(charInorder)):
-            c = charInorder[i]
-            _charvalue[c] = i * 2
-            if c != c.upper():
-                _charvalue[c.upper()] = i * 2 - 1
-        del charInorder
-    mult = len(_charvalue)
+_char_value = {}
+char_in_order = [
+    '#', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'ā',
+    'i', 'ī', 'u', 'ū', 'e', 'o', 'ṁ', 'ṃ', 'k', 'kh', 'g', 'gh', 'ṅ', 'c',
+    'ch', 'j', 'jh', 'ñ', 'ṭ', 'ṭh', 'ḍ', 'ḍh', 'ṇ', 't', 'th', 'd',
+    'dh', 'n', 'p', 'ph', 'b', 'bh', 'm', 'y', 'r', 'l', 'ḷ', 'v', 's', 'h',
+]
+for i, c in enumerate(char_in_order):
+    _char_value[c] = i * 2
+    if c != c.upper():
+        _char_value[c.upper()] = i * 2 - 1
+        if len(c) > 1:
+            _char_value[c.title()] = i * 2 - 1
+del char_in_order
+
+def pali_sort_key(string):
+    """sorts strings into pali alphabetical order"""
+    
     vals = []
-    for i in range(0, len(input)):
-        val = 0
-        c1 = input[i]
-        c2 = input[i:i+2]
-        if c2 in _charvalue:
-            val = _charvalue[c2]
-            i += 1
-        elif c1 in _charvalue:
-            val = _charvalue[c1]
-        vals.append(val)
+    i = 0
+    while i < len(string):
+        c1 = string[i]
+        c2 = string[i:i+2]
+        if c2 in _char_value:
+            val = _char_value[c2]
+            i += 1 # advance an extra character
+        elif c1 in _char_value:
+            val = _char_value[c1]
+        else:
+            val = 0
+        vals.append(val)    
+        i += 1
+    
     return tuple(vals)
 
-def asciify_roman(string, _rexdia=regex.compile(r'\p{dia}')):
+def asciify_roman(string):
     out = unicodedata.normalize('NFD', string)
-    out = _rexdia.sub('', out)
+    out = regex.sub(r'\p{dia}', '', out)
     out = out.replace('\xad', '')
     return out
 
