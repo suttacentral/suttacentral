cf7c2b09066f442d470962797d854a8e88bcba6f
Blake Walsh <blakeofthepath@gmail.com>
2018-01-02 13:48:43 +0200

    Updated code to include a number representing the dictionary order and precalculated similar terms

--- server/server/data_loader/dictionary_full.py
+++ server/server/data_loader/dictionary_full.py
@@ -1,13 +1,16 @@
 import json
 from tqdm import tqdm
 from collections import Counter
-from textfunctions import asciify, palisortkey
+from .textfunctions import asciify_roman, pali_sort_key
+from .fuzzymatcher import FuzzyMatcher
 
 def load_dictionary_full(db, dictionaries_dir, change_tracker):
     print('Loading dictionary_full')
     
     dictionary_full_files = list((dictionaries_dir / 'en').glob('*.json'))
-    if change_tracker.is_any_file_new_or_changed(dictionary_full_files)
+    if not change_tracker.is_any_file_new_or_changed(dictionary_full_files):
+        return
+    
     dictionary_full_collection = db['dictionary_full']
     docs = []
     # For now hardcode this, in the future we may have other pairs
@@ -21,7 +24,8 @@ def load_dictionary_full(db, dictionaries_dir, change_tracker):
                 word = entry['word'].lower()
                 words_seen[word] += 1
                 
-                _id = asciify(word)
+                # create a meaningful id
+                _id = asciify_roman(word)
                 if _id in ids_seen:
                     _id += str(ids_seen[_id])
                 ids_seen[_id] += 1
@@ -38,8 +42,10 @@ def load_dictionary_full(db, dictionaries_dir, change_tracker):
     words_sorted = sorted(words_seen, key=pali_sort_key)
     word_number = {w: i for i, w in enumerate(words_sorted)}
     
-    for doc in docs:
+    fm = FuzzyMatcher(words_seen)
+    for doc in tqdm(docs):
         doc['num'] = word_number[doc['word']]
+        doc['similar'] = [t[0] for t in fm.search(doc['word'])]
     
     dictionary_full_collection.truncate()
     dictionary_full_collection.import_bulk(docs)
