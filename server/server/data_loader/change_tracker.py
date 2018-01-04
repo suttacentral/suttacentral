ce1caa9fa5c21b7086e1ecdf373ef199807ac57b
Blake Walsh <blakeofthepath@gmail.com>
2018-01-04 13:56:42 +0200

    Added functionality to ChangeTracker that can detect when the source code for a function has changed, this can be used to trigger executing code even if the data hasn't changed. This is partly for ease of iteration but mainly so there's no need to inform others to do a full rebuild.

--- server/server/data_loader/change_tracker.py
+++ server/server/data_loader/change_tracker.py
@@ -1,8 +1,17 @@
+import hashlib
+import inspect
+
 class ChangeTracker:
     def __init__(self, base_dir, db):
         self.base_dir = base_dir
         self.db = db
-
+        
+        self.old_function_hashes = {doc['_key']: doc['hash'] for doc in db.aql.execute('''
+            FOR doc IN function_hashes
+                RETURN doc
+        ''')}
+        self.new_function_hashes = {}
+        
         # Extract the mtimes from arangodb
 
         self.old_mtimes = old_mtimes = {entry['path']: entry['mtime'] for entry in db.aql.execute('''
@@ -34,6 +43,22 @@ class ChangeTracker:
 
     def is_any_file_new_or_changed(self, files):
         return any(self.is_file_new_or_changed(file) for file in files)
+    
+    def is_any_function_changed(self, functions):
+        return any(self.is_function_changed(function) for function in functions)
+    
+    def is_function_changed(self, function):
+        key = f'{function.__module__}.{function.__qualname__}'
+        
+        # Generate a hash for the function using the source lines
+        # this is obviously not foolproof
+        function_source = ''.join(inspect.getsourcelines(function)[0])
+        function_hash = hashlib.md5(function_source.encode()).hexdigest()
+        
+        self.new_function_hashes[key] = function_hash
+        if self.old_function_hashes.get(key) == function_hash:
+            return False
+        return True        
 
     def update_mtimes(self):
         # Update mtimes in arangodb
@@ -45,3 +70,5 @@ class ChangeTracker:
         self.db['mtimes'].import_bulk(
             [{'path': k, 'mtime': v, '_key': k.replace('/', '_')} for k, v in
              self.changed_or_new.items()], on_duplicate="replace")
+        self.db['function_hashes'].truncate()
+        self.db['function_hashes'].import_bulk([{'_key': k, 'hash': v} for k, v in self.new_function_hashes.items()])
