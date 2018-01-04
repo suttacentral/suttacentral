f8b8808e8ab0de083454c79efbabe1c68d2eca45
Blake Walsh <blakeofthepath@gmail.com>
2018-01-04 14:32:23 +0200

    Figured since I'm using inspect may as well go full evil and also introspect the calling function. Made this automatic by default for the purpose of consistency, but also easy to switch off.

--- server/server/data_loader/change_tracker.py
+++ server/server/data_loader/change_tracker.py
@@ -1,5 +1,6 @@
 import hashlib
 import inspect
+import gc
 
 class ChangeTracker:
     def __init__(self, base_dir, db):
@@ -36,13 +37,19 @@ class ChangeTracker:
         print(f'{len(changed_or_new)} files to be processed')
         print(f'{len(deleted)} files to be deleted')
 
-    def is_file_new_or_changed(self, path):
+    def is_file_new_or_changed(self, path, check_calling_function=True):
+        if check_calling_function:
+            if self.is_function_changed(who_is_calling()):
+                return True
         if str(path.relative_to(self.base_dir)) in self.changed_or_new:
             return True
         return False
 
-    def is_any_file_new_or_changed(self, files):
-        return any(self.is_file_new_or_changed(file) for file in files)
+    def is_any_file_new_or_changed(self, files, check_calling_function=True):
+        if check_calling_function:
+            if self.is_function_changed(who_is_calling()):
+                return True
+        return any(self.is_file_new_or_changed(file, False) for file in files)
     
     def is_any_function_changed(self, functions):
         return any(self.is_function_changed(function) for function in functions)
@@ -52,8 +59,8 @@ class ChangeTracker:
         
         # Generate a hash for the function using the source lines
         # this is obviously not foolproof
-        function_source = ''.join(inspect.getsourcelines(function)[0])
-        function_hash = hashlib.md5(function_source.encode()).hexdigest()
+        
+        function_hash = hashlib.md5(function_source(function).encode()).hexdigest()
         
         self.new_function_hashes[key] = function_hash
         if self.old_function_hashes.get(key) == function_hash:
@@ -72,3 +79,31 @@ class ChangeTracker:
              self.changed_or_new.items()], on_duplicate="replace")
         self.db['function_hashes'].truncate()
         self.db['function_hashes'].import_bulk([{'_key': k, 'hash': v} for k, v in self.new_function_hashes.items()])
+        
+
+
+def function_source(function):
+    return ''.join(inspect.getsourcelines(function)[0])
+
+def who_is_calling(depth=2):
+    """Return the object for the executing function at given stack depth
+    
+    adapted from https://stackoverflow.com/a/4506081/4092906
+    """
+    frame_info = inspect.stack()[depth]
+    frame = frame_info.frame
+    code  = frame.f_code
+    globs = frame.f_globals
+    functype = type(lambda: 0)
+    funcs = []
+    for func in gc.get_referrers(code):
+        if type(func) is functype:
+            if getattr(func, "__code__", None) is code:
+                if getattr(func, "__globals__", None) is globs:
+                    funcs.append(func)
+                    if len(funcs) > 1:
+                        return None
+    return funcs[0] if funcs else None
+
+def whoami():
+    return who_is_calling(depth=2)
