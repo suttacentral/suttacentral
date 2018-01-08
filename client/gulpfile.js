c01716463c584d2a70d91693bd2ce378025955a2
Hubert <hubert.dworczynski@gmail.com>
2018-01-09 00:00:45 +0100

    wip

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -5,14 +5,14 @@ const i18nPreprocess = require('gulp-i18n-preprocess');
 let attributesRepository; // constructed attributes repository
 
 // Other standard pipes such as crisper / minification / uglification are omitted for explanation
-gulp.task('preprocess', function () {
-    const elements = gulp.src(['elements/static/**/*.html']) // input custom element HTMLs
+gulp.task('preprocess', function() {
+    const elements = gulp.src(['elements/static-templates/**/*.html']) // input custom element HTMLs
         .pipe(i18nPreprocess({
             replacingText: true, // replace UI texts with {{annotations}}
             jsonSpace: 2, // JSON format with 2 spaces
             attributesRepository: attributesRepository // input attributes repository
         }))
-        .pipe(gulp.dest('localization/i18nOutput')); // output preprocessed HTMLs and default JSON files to dist
+        .pipe(gulp.dest('localization/preprocessedStaticTemplates')); // output preprocessed HTMLs and default JSON files to dist
 
     return elements;
 });
