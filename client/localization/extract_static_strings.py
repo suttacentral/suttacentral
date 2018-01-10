e05f218ce0930596cffa7a0200bf6c91dbaa8ac4
Jakub Semik <kuba.semik@gmail.com>
2018-01-10 10:51:37 +0100

    PR chagnes

--- client/localization/extract_static_strings.py
+++ client/localization/extract_static_strings.py
@@ -12,7 +12,7 @@ TEMPLATE_DIR = '../elements/static-templates/'
 STATIC_DIR = '../elements/static/'
 LOCALIZATION_JSONS_DIR = '../localization/elements/static/'
 
-NON_BLOCK_ELEMENTS = ['a', 'span', '\n', 'code', 'br', 'cite', 'strong', 'i']
+NON_BLOCK_ELEMENTS = ['a', 'span', '\n', 'code', 'br', 'cite', 'strong', 'i', 'em']
 EXCLUDE_BLOCKS = ['style']
 
 
