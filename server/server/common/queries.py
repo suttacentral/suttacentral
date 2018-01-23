65266ed8300aff3a35390c3e94454bb0b901ed8e
SamaneriVimala <samaneri.vimala@gmail.com>
2018-01-23 09:18:02 -0800

    [SUCE-456] vinaya parallels display

--- server/server/common/queries.py
+++ server/server/common/queries.py
@@ -533,7 +533,7 @@ RETURN similar_words
 EXPANSION = '''
 LET expansion_item = (
     FOR entry IN uid_expansion
-        RETURN { [ entry.uid ]: entry.acro }
+        RETURN { [ entry.uid ]: [ entry.acro, entry.name ] }
     )
     
 RETURN MERGE(expansion_item)
