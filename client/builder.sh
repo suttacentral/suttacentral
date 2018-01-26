bfebfa6c81eac4c6374756a3c26fdb2b7384e8d9
Hubert <hubert.dworczynski@gmail.com>
2018-01-26 10:47:40 +0100

    PR changes

--- client/builder.sh
+++ client/builder.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+set -e
 echo "BUILDING FRONTEND FOR PRODUCTION"
 npm install -g polymer-cli --unsafe-perm
 polymer build --verbose
 npm install
+echo "\033[1;32mFRONTEND BUILT SUCCESSFULLY!"
