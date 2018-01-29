5ed09af0cf7325a47633cc080030bd1354b1eb73
Hubert <hubert.dworczynski@gmail.com>
2018-01-29 14:18:16 +0100

    print status

--- wait_for_builder.sh
+++ wait_for_builder.sh
@@ -5,6 +5,7 @@ status=1
 while [ ${status} != 0 ]; do
     docker exec sc-frontend-builder cat /tmp/.done.info 2> /dev/null
     status=$?
+    echo `status`
     if [ ${status} != 0 ]; then
         sleep 0.5
     fi
