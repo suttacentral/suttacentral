2a2b65a2387123aa4e20b5f74143a36b70dfbfce
Hubert <hubert.dworczynski@gmail.com>
2018-01-29 15:07:35 +0100

    change status variable name

--- wait_for_builder.sh
+++ wait_for_builder.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # Wait until frontend has finished building
 
-status=1
-while [ ${status} != 0 ]; do
+exit_status=1
+while [ ${exit_status} != 0 ]; do
     docker exec sc-frontend-builder cat /tmp/.done.info 2> /dev/null
-    status=$?
+    exit_status=$?
     echo `status`
-    if [ ${status} != 0 ]; then
+    if [ ${exit_status} != 0 ]; then
         sleep 0.5
     fi
 done
