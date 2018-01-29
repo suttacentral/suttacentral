728dd776758ce91997cff2ae361e4e3426b50ed4
Hubert <hubert.dworczynski@gmail.com>
2018-01-29 15:36:16 +0100

    print actual variable

--- wait_for_builder.sh
+++ wait_for_builder.sh
@@ -5,7 +5,7 @@ exit_status=1
 while [ ${exit_status} != 0 ]; do
     docker exec sc-frontend-builder cat /tmp/.done.info 2> /dev/null
     exit_status=$?
-    echo `exit_status`
+    echo ${exit_status}
     if [ ${exit_status} != 0 ]; then
         sleep 0.5
     fi
