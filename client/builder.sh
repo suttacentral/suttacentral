fcae91b18c2c6b3e42ba0365fcee2d937cd2a6fa
Hubert <hubert.dworczynski@gmail.com>
2018-02-01 00:37:58 +0100

    make workbox work

--- client/builder.sh
+++ client/builder.sh
@@ -3,3 +3,5 @@ echo "BUILDING FRONTEND FOR PRODUCTION"
 npm install
 npm install -g polymer-cli --unsafe-perm
 polymer build --verbose
+rm -rf ./build/default/node_modules/
+cp -r ./node_modules ./build/default/
