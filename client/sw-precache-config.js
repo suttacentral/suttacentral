8bda1a5ef9874ba8cb24a9c2970ccb2d10d80857
Hubert <hubert.dworczynski@gmail.com>
2018-01-31 15:29:55 +0100

    add sw-precache-config

---
+++ client/sw-precache-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    staticFileGlobs: [
+        './index.html',
+        './manifest.json',
+        '/bower_components/webcomponentsjs/webcomponents-lite.js',
+        '/files/**/*',
+        'img/**/*',
+        'localization/**/*'
+    ],
+    navigateFallback: './index.html'
+};
