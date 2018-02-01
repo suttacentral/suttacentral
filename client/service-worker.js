ed6e512480f28b99163e9053b8a23980530048f7
Hubert <hubert.dworczynski@gmail.com>
2018-02-01 11:53:58 +0100

    wip

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,3 +1,19 @@
 importScripts('/node_modules/workbox-sw/build/importScripts/workbox-sw.dev.v2.1.2.js');
 
+const staticAssets = [
+    './',
+    './manifest.json',
+    '/bower_components/webcomponentsjs/webcomponents-lite.js',
+    'img/home-page/*',
+    'img/static-pages/*',
+    'img/*.svg',
+    'img/favicon-32x32.png',
+    'img/favicon.ico',
+    'img/*.html',
+    'localization/elements/**/*'
+];
+
 const workboxSW = new WorkboxSW();
+workboxSW.precache(staticAssets);
+
+workboxSW.router.registerRoute('http://localhost/api/(.*)', workboxSW.strategies.networkFirst());
