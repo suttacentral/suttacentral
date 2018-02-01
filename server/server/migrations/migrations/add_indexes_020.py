ab793bcb00e2c73efb5f256b5383760057a16509
Blake Walsh <blakeofthepath@gmail.com>
2018-02-01 13:06:25 +0200

    Added some missing indexes which marginally impaired performance of certain queries

---
+++ server/server/migrations/migrations/add_indexes_020.py
@@ -0,0 +1,23 @@
+from common.arangodb import get_db
+from migrations.base import Migration
+
+
+class SecondMigration(Migration):
+    migration_id = 'add_indexes_020'
+    tasks = ['add_indexes']
+
+    def add_indexes(self):
+        db = get_db()
+
+        po_strings = db['po_strings']
+        po_strings.add_hash_index(fields=['uid'], unique=False)
+        po_strings.add_hash_index(fields=['lang'], unique=False)
+        
+        po_markup = db['po_markup']
+        po_markup.add_hash_index(fields=['uid'], unique=False)
+        
+        language = db['language']
+        language.add_hash_index(fields=['uid'], unique=True)
+        language.add_hash_index(fields=['is_root'], unique=False)
+        
+        
