1befcef62a194fdf54037fe0f41b0e3dc5be89fa
Hubert Dworczyński <hubert.dworczynski@gmail.com>
2018-02-02 11:05:57 +0100

    [SUCE-96] Service worker set up (#467)
    * add sw-precache-config
    
    * delete redundant icons
    
    * don't add Polymer service worker
    
    * add workbox-sw dependency
    
    * add workbox service worker
    
    * make workbox work
    
    * wip
    
    * compress life_of_buddha image
    
    * create workbox-cli-config.js
    
    * delete paper-styles
    
    * Revert "delete paper-styles"
    
    This reverts commit b67584457c465781480da70c69981594d340bf27.
    
    * cache all requests in service worker
    
    * add registerNavigationRoute
    
    * cache Stripe requests
    
    * whitespace changes
    
    * improve stripe route regex
    
    * fix syntax

---
+++ client/service-worker.js
@@ -0,0 +1,37 @@
+importScripts('/sw-generated.js');
+
+const sw = new WorkboxSW();
+
+// Cache API requests
+sw.router.registerRoute(
+    new RegExp('http://localhost/api/(.*)'),
+    sw.strategies.networkFirst()
+);
+
+sw.router.registerRoute(
+    new RegExp('https://next.suttacentral.com/api/(.*)'),
+    sw.strategies.networkFirst()
+);
+
+// Cache Google fonts
+sw.router.registerRoute(
+    new RegExp('^https://fonts.(?:googleapis|gstatic).com/(.*)'),
+    sw.strategies.cacheFirst()
+);
+
+// Cache Stripe scripts
+sw.router.registerRoute(
+    new RegExp('^https://(?:js|m).stripe.com/(.*)'),
+    sw.strategies.cacheFirst()
+);
+
+// Register navigation routes:
+sw.router.registerNavigationRoute('.', {
+    whitelist: [
+        /^\/$/,
+        /^\/*/
+    ],
+    blacklist: [
+        /^\/api/
+    ]
+});
