55fc289c828cd3d1d0182db80dc27e98a8872dcf
Hubert <hubert.dworczynski@gmail.com>
2018-03-08 00:49:11 +0100

    delete comments

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,6 +1,4 @@
 const isProductionEnv = !self.location.hostname.match(/127.0.0.1|localhost|172[\d.]+/);
-console.log(self.location.hostname);
-console.log('isProductionEnv? ', isProductionEnv);
 
 if (isProductionEnv) {
     importScripts('/node_modules/workbox-sw/build/importScripts/workbox-sw.prod.v2.1.2.js');
@@ -40,6 +38,5 @@ sw.router.registerRoute(
 // This returns the cached value for '/' (index.html) when the user requests a URL like suttacentral.net/home
 // instead of just looking for the cached match for suttacentral.net/home, which doesn't exist.
 if (isProductionEnv) {
-    console.log('registering navigation route');
     sw.router.registerNavigationRoute('index.html');
 }
