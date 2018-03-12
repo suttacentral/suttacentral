847ac40be2bb8c0436c7eea5112cd3a8a762cb2f
Hubert <hubert.dworczynski@gmail.com>
2018-03-12 12:04:51 +0100

    fix sw navigation route

--- client/service-worker.js
+++ client/service-worker.js
@@ -38,5 +38,15 @@ sw.router.registerRoute(
 // This returns the cached value for '/' (index.html) when the user requests a URL like suttacentral.net/home
 // instead of just looking for the cached match for suttacentral.net/home, which doesn't exist.
 if (isProductionEnv) {
-    sw.router.registerNavigationRoute('index.html');
+    caches.keys().then(keys => {
+        const cacheName = keys[0];
+        caches.open(cacheName).then(cache => {
+            if (cache.match('/')) {
+                sw.router.registerNavigationRoute('/');
+            }
+            else if (cache.match('index.html')) {
+                sw.router.registerNavigationRoute('index.html');
+            }
+        });
+    });
 }
