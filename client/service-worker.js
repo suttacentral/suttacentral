ca43ad55f92061f597d2708927fdf3fa7486a063
Jakub Semik <kuba.semik@gmail.com>
2018-03-16 12:56:00 +0100

    reset download after page close

--- client/service-worker.js
+++ client/service-worker.js
@@ -15,12 +15,12 @@ sw.precache([]);
 // Cache API requests
 sw.router.registerRoute(
     new RegExp('http://localhost/api/(.*)'),
-    sw.strategies.networkFirst()
+    sw.strategies.staleWhileRevaliate()
 );
 
 sw.router.registerRoute(
     new RegExp('https://next.suttacentral.net/api/(.*)'),
-    sw.strategies.networkFirst()
+    sw.strategies.staleWhileRevaliate()
 );
 
 // Cache Google fonts
