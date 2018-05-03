6a7e8312455448b8eeecba89bf162dfc24bb71bb
Blake Walsh <blakeofthepath@gmail.com>
2018-05-03 14:07:52 +0300

    Allow installing arbitary certificates for SSL

--- nginx/create-certs.sh
+++ nginx/create-certs.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-echo "Creating self-signed certificate"
-mkdir -p /opt/sc/certs
-openssl req -x509 -nodes -days 50000 -newkey rsa:2048 \
-    -subj "/O=SuttaCentral" \
-    -keyout  /opt/sc/certs/self-signed.key \
-    -out /opt/sc/certs/self-signed.crt
+PEM_FILE=/etc/nginx/configurations/certs/server.pem
+KEY_FILE=/etc/nginx/configurations/certs/server.key
+
+if [ ! -f $PEM_FILE ]; then
+    echo "Creating self-signed certificate"
+    openssl req -x509 -nodes -days 50000 -newkey rsa:2048 \
+        -subj "/O=SuttaCentral" \
+        -keyout $KEYFILE \
+        -out $PEM_FILE
+fi
 
