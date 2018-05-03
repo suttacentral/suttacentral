891f539cef60d73007740c35f9637dc0cbe5d295
Blake Walsh <blakeofthepath@gmail.com>
2018-05-03 15:17:27 +0300

    Changed create-certs to a host function

--- nginx/create-certs.sh
+++ nginx/create-certs.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 
-if [ ! -f /etc/nginx/configurations/certs/server.pem ]; then
+if [ ! -f ./conf.d/certs/server.pem ]; then
     echo "Creating self-signed certificate"
     openssl req -x509 -nodes -days 50000 -newkey rsa:2048 \
         -subj "/O=SuttaCentral" \
-        -keyout /etc/nginx/configurations/certs/server.pem \
-        -out /etc/nginx/configurations/server.key
+        -keyout ./conf.d/certs/server.pem \
+        -out ./conf.d/server.key
 fi
 
