53f36b25d7fff5bd2ce8dae6eb5b6cb2daf40d03
Blake Walsh <blakeofthepath@gmail.com>
2018-05-23 13:44:05 +0300

    Prevent pootle from redoing init steps when only the container is rebuilt.

--- pootle/pootle.sh
+++ pootle/pootle.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
-if [ ! -f /root/.pootle/.migrate ]; then
+if [ ! -f /home/pootle/.locks/.migrate ]; then
 	sleep 20
 fi
 
 sleep 5
 
-if [ ! -f /root/.pootle/.migrate ]; then
+if [ ! -f /home/pootle/.locks/.migrate ]; then
     echo "run migrate"
     pootle migrate
     pootle initdb --no-projects
     echo "from django.contrib import auth; auth.get_user_model()._default_manager.db_manager(\"default\").create_superuser(\"$POOTLE_ADMIN_NAME\", \"$POOTLE_ADMIN_EMAIL\", \"$POOTLE_ADMIN_PASSWORD\")" | /usr/local/bin/pootle shell
     pootle verify_user "$POOTLE_ADMIN_NAME"
-    touch /root/.pootle/.migrate
+    touch /home/pootle/.locks/.migrate
 fi
 
-if [ ! -f /root/.pootle/.assets ]; then
+if [ ! -f /home/pootle/.locks/.assets ]; then
     echo "build assets"
     cd /usr/local/lib/python2.7/dist-packages/pootle/static/js && npm install
     pootle collectstatic --noinput
     pootle webpack
     pootle assets build
-    touch /root/.pootle/.assets
+    touch /home/pootle/.locks/.assets
 fi
 
-if [ ! -f /root/.pootle/.clonedAssets ]; then
+if [ ! -f /home/pootle/.locks/.clonedAssets ]; then
     echo "cloning repository to build more assets"
     git clone https://github.com/suttacentral/pootle.git
     cd pootle
     make assets
     echo "assets built"
-    touch /root/.pootle/.clonedAssets
+    touch /home/pootle/.locks/.clonedAssets
 fi
 
 mkdir log
