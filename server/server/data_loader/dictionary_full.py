df1f84411590a2c55351963511ba56fd1046ee8b
Blake Walsh <blakeofthepath@gmail.com>
2018-09-20 17:53:56 +0300

    This fixes an error which was exposed by updates to python-arango (the error was already there, just silent)

--- server/server/data_loader/dictionary_full.py
+++ server/server/data_loader/dictionary_full.py
@@ -1,4 +1,5 @@
 import json
+import regex
 from tqdm import tqdm
 from collections import Counter
 from .textfunctions import asciify_roman, pali_sort_key
@@ -27,13 +28,13 @@ def load_dictionary_full(db, dictionaries_dir, change_tracker):
             
             # create a meaningful id
             word_ascii = asciify_roman(word)
-            _id = word_ascii
-            if _id in ids_seen:
-                _id += str(ids_seen[_id])
-            ids_seen[_id] += 1
+            _key = regex.sub(r'[^a-z0-9]+', '_', word_ascii)
+            if _key in ids_seen:
+                _key += str(ids_seen[_key])
+            ids_seen[_key] += 1
             words_seen[word] += 1
             
-            doc = {'_id': _id,
+            doc = {'_key': _key,
                    'dictname': dictionary.stem,
                    'lang_to': lang_to,
                    'lang_from': lang_from,
@@ -43,12 +44,19 @@ def load_dictionary_full(db, dictionaries_dir, change_tracker):
                    }
             docs.append(doc)
     
+    seen = set()
+    for doc in docs:
+        if doc['_key'] in seen:
+            print(f'Duplicate: {doc["_key"]}')
+            seen.add(doc['_key'])
+    
     words_sorted = sorted(words_seen, key=pali_sort_key)
     word_number = {w: i for i, w in enumerate(words_sorted)}
     
+    print('Generating fuzzy matches: this might take some time')
     fm = FuzzyMatcher(words_seen)
     for doc in tqdm(docs):
         doc['num'] = word_number[doc['word']]
         doc['similar'] = [t[0] for t in fm.search(doc['word'])]
     
-    dictionary_full_collection.import_bulk(docs, overwrite=True)
+    dictionary_full_collection.import_bulk(docs, overwrite=True, on_duplicate="ignore")
