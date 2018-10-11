31a801ce03c2b927ea6eb1ff0d6c922f66bec7e5
Jan Latour <jan.latour@stxnext.pl>
2018-10-12 01:44:05 +0200

    Upgrade application shell

---
+++ client/constants.js
@@ -0,0 +1,4 @@
+const currentUrl = window.location;
+export const API_ROOT = `${currentUrl.protocol}//${currentUrl.host}/api`;
+
+export const IMAGES_ROOT = 'https://legacy.suttacentral.net/text_images';
