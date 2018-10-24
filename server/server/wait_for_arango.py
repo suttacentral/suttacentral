042e2e7fd62f27a9843ddc38bcf7a72459cdabd9
Jan Latour <jan.latour@stxnext.pl>
2018-10-24 09:49:25 +0200

    Changes for python-arango version 4 vol. 3

--- server/server/wait_for_arango.py
+++ server/server/wait_for_arango.py
@@ -2,23 +2,27 @@ import os
 import time
 
 from arango import ArangoClient
-from arango.exceptions import DatabaseListError
-from requests.exceptions import ConnectionError
+from arango.exceptions import ServerConnectionError
 
 
 def connect():
     client = ArangoClient(
         host=os.getenv('ARANGO_HOST'),
-        port=os.getenv('ARANGO_PORT'),
+        port=os.getenv('ARANGO_PORT')
+    )
+
+    # arango client >= 4 thows only when verify is set to True
+    client.db(
+        '_system',
         username=os.getenv('ARANGO_USER'),
         password=os.getenv('ARANGO_ROOT_PASSWORD'),
+        verify=True
     )
-    client.databases()
 
 
 while True:
     try:
         connect()
         break
-    except (ConnectionError, DatabaseListError):
+    except ServerConnectionError:
         time.sleep(1)
