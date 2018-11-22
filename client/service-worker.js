2d30e7f57b7bcfbfc5e45270232886d8d36b29fa
Robert Musił <robert.musil@stxnext.pl>
2018-11-22 15:33:20 +0100

    Fixed caching on the production env

--- client/service-worker.js
+++ client/service-worker.js
@@ -19,13 +19,13 @@ sw.router.registerRoute(
 );
 
 sw.router.registerRoute(
-    new RegExp('https://(?:staging.)suttacentral.net/api/(.*)'),
+    new RegExp('https://(?:staging\.)?suttacentral.net/api/(.*)'),
     sw.strategies.networkFirst()
 );
 
 // Cache assets (images and fonts)
 sw.router.registerRoute(
-    new RegExp('https://(?:staging.)suttacentral.net/(?:img|files)/(.*)'),
+    new RegExp('https://(?:staging\.)?suttacentral.net/(?:img|files)/(.*)'),
     sw.strategies.cacheFirst({
         cacheName: "assets",
         cacheExpiration: {
@@ -52,7 +52,7 @@ if (isProductionEnv) {
     // This returns the cached value for '/' (index.html) when the user requests a URL like suttacentral.net/home
     // instead of just looking for the cached match for suttacentral.net/home, which doesn't exist.
     caches.keys().then(keys => {
-        const cacheName = keys.filter(name => name.includes('suttacentral'))[0];
+        const cacheName = keys.find(name => name.includes('suttacentral'));
         caches.open(cacheName).then(cache => {
             const cacheOptions = {
                 blacklist: [
