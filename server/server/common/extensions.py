08c938967161c2ea08b2818d88779a4177b3bdeb
Blake Walsh <blakeofthepath@gmail.com>
2019-01-30 21:08:12 +0200

    Increased cache timeout and use of cache

--- server/server/common/extensions.py
+++ server/server/common/extensions.py
@@ -1,7 +1,7 @@
 from flask_caching import Cache
 from flask import request
 
-cache = Cache()
+cache = Cache(config={'CACHE_TYPE': 'simple'})
 
 def make_cache_key(*args, **kwargs):
     return request.url.encode('utf-8')
