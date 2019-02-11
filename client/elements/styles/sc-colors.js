9e30cba066011a47dd7f610e826a79eb9c58e675
sujato <sujato@gmail.com>
2019-02-11 21:14:24 +1100

    more tweaks to colors

--- client/elements/styles/sc-colors.js
+++ client/elements/styles/sc-colors.js
@@ -26,7 +26,7 @@ template.innerHTML = `
       --sc-secondary-accent-color: #9c27b0;
 
       /* Background colors */
-      --sc-primary-background-color: rgb(250, 249, 249);
+      --sc-primary-background-color: rgb(250, 249, 248);
       --sc-secondary-background-color: rgb(255, 255, 255);
       --sc-tertiary-background-color: rgb(244, 243, 242);
 
