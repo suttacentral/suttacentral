b9e8b7d212fd943137c37c3a8bce799b534eb2dc
ihongda <hd2935@qq.com>
2019-10-07 09:22:59 +0800

    fix: Taisho parallels table has missing column letters #1477

--- client/utils/suttaplex.js
+++ client/utils/suttaplex.js
@@ -8,7 +8,8 @@ export function transformId(rootId, expansionData, idOrName = 0) {
       const idNumbers = rootId.substring(5);
       return `G-3 Dhp ${idNumbers}`;
     }
-    const idPart = getParagraphRange(rootId).replace('--', '–').replace('#', ': ').replace(/[a-z]+/g,'');
+    //const idPart = getParagraphRange(rootId).replace('--', '–').replace('#', ': ').replace(/[a-z]+/g,'');
+    const idPart = getParagraphRange(rootId).replace('--', '–').replace('#', ': ');
     let scAcronym = '';
     const rootPart = rootId.split('#')[0];
     const uidParts = rootPart.split('-');
