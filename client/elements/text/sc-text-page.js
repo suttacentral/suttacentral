c76bd922f011d4e2ed0cebc43c8b32ab87c98f19
ihongda <hd2935@qq.com>
2020-03-29 16:31:15 +0800

    Undo the override of the sc-text-page component.

--- client/elements/text/sc-text-page.js
+++ client/elements/text/sc-text-page.js
@@ -1,23 +1,26 @@
-import {LitElement, html} from 'lit-element';
+import { PolymerElement } from '@polymer/polymer/polymer-element.js';
 import '@polymer/iron-a11y-keys/iron-a11y-keys.js';
-import { LitLocalized } from '../addons/localization-mixin';
 
-export class SCTextPage extends LitLocalized(LitElement) {
+import { Localized } from '../addons/localization-mixin.js';
+import { ReduxMixin } from '../../redux-store.js';
+
+
+export class SCTextPage extends ReduxMixin(Localized(PolymerElement)) {
   static get properties() {
     return {
-      inputElement: { type: Object },
-      showParagraphs: { type: Boolean },
-      chosenTextView: { type: String },
-      lang: { type: String }
+      inputElement: {
+        type: Object
+      },
+      showParagraphs: {
+        type: Boolean,
+      },
+      chosenTextView: {
+        type: String,
+      },
+      lang: {
+        type: String
+      }
     }
-  }
-
-  constructor() {
-    super();
-    this.inputElement = {};
-    this.showParagraphs = false;
-    this.chosenTextView = '';
-    this.lang = '';
   }
 
   deathToTheBeast() {
