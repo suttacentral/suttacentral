0829f71918b9d8e1d47766887f401ace517bfec8
ihongda <hd2935@qq.com>
2020-04-25 09:59:49 +0800

    Recover the code that detects whether the file has been modified.

--- server/server/data_loader/segmented_data.py
+++ server/server/data_loader/segmented_data.py
@@ -8,15 +8,15 @@ def load_segmented_data(db, change_tracker, segmented_data_dir):
             continue
         files.append(file)
         
-    # if change_tracker.is_any_file_new_or_changed(files):
-    for file in files:
-        uid, muids = file.stem.split('_')
-        docs.append({
-            '_key': file.stem,
-            'uid': uid,
-            'muids': muids.split('-'),
-            'filepath': str(file.relative_to(segmented_data_dir))
-        })
+    if change_tracker.is_any_file_new_or_changed(files):
+        for file in files:
+            uid, muids = file.stem.split('_')
+            docs.append({
+                '_key': file.stem,
+                'uid': uid,
+                'muids': muids.split('-'),
+                'filepath': str(file.relative_to(segmented_data_dir))
+            })
     
     print(f'{len(docs)} files added or updated')
     db['segmented_data'].truncate()
