0fe8f5e92b1e679cbd55df03537f09b2af7318f8
sujato <sujato@gmail.com>
2020-05-03 10:56:44 +1000

    final refinements for text styles

--- client/elements/styles/sc-bilara-segmented-text-styles.js
+++ client/elements/styles/sc-bilara-segmented-text-styles.js
@@ -17,209 +17,219 @@ export const commonStyles = css`
     --sc-size-xxl: 64px;
      font-size: var(--sc-skolar-font-size-md);
   }
-  * {
-  margin: 0
-}
 
-  main {
+main {
     display: flex;
     justify-content: center;
-    background-color: var(--sc-tertiary-background-color);
     color: var(--sc-primary-text-color);
+    margin: 4em 2em;
+}
 
-          }
-          article{
-                margin: 4em 1em ;
-          }
-  header {
+/* text title */
+
+header {
     text-align: center;
     color: var(--sc-secondary-text-color);
     margin: 4em 0;
-  }
-  header li span.text
-  {
+}
+header ul {
+    padding: 0;
+    list-style-type: none;
     font-variant-caps: all-small-caps;
     font-weight: normal;
     font-family: var(--sc-sans-font);
-  }
-
-
-header ul{
-  margin: 0.7em 0;
-  list-style-type: none;
 }
-
-h2, h3, h4, h5, h6 {
-  margin: 1em 0 0 0;
-  color: var(--sc-secondary-text-color);
-}
-p, ul, ol{
-  margin: 0.75em 0 0 0;
-  line-height: 1.5;
-  font-family: var(--sc-serif-font);
-  font-size: 1em;
-  padding: 0
-}
-/* font sizes */
-h1, h2, h3, h4, h5, h6{
+h1 {
     line-height: 1.3333;
+    margin: 0.5em 0 0 0;
+    color: var(--sc-secondary-text-color);
+    font-family: var(--sc-serif-font);
+    font-size: 2em;
+    font-size: clamp(1.5em, 5vw, 2em);
+    font-weight: 300;
+    font-variant-caps: small-caps;
+    text-align: center;
+}
+
+/* text general */
+
+h2 {
+    line-height: 1.3333;
+    margin: 1em 0 0 0;
+    color: var(--sc-secondary-text-color);
+    font-family: var(--sc-serif-font);
+    font-size: 1.5em;
+    font-size: clamp(1.125em, 3.75vw, 1.5em);
+    font-weight: 400;
+}
+h3 {
+    line-height: 1.3333;
+    margin: 1em 0 0 0;
+    color: var(--sc-secondary-text-color);
+    font-family: var(--sc-serif-font);
+    font-size: 1.333em;
+    font-size: clamp(1em, 3.333vw, 1.333em);
+    font-weight: 400;
+}
+h4 {
+    line-height: 1.3333;
+    margin: 1em 0 0 0;
+    color: var(--sc-secondary-text-color);
+    font-family: var(--sc-serif-font);
+    font-size: 1.25em;
+    font-size: clamp(1em, 5vw, 1.25em);
+    font-weight: 400;
+}
+h5 {
+    line-height: 1.3333;
+    margin: 1em 0 0 0;
+    color: var(--sc-secondary-text-color);
+    font-family: var(--sc-serif-font);
+    font-size: 1em;
+    font-weight: 600;
+}
+h6 {
+    line-height: 1.3333;
+    margin: 1em 0 0 0;
+    color: var(--sc-secondary-text-color);
+    font-family: var(--sc-serif-font);
+    font-size: 1em;
+    font-weight: 400;
+    font-style: italic
+}
+p,
+ul,
+ol {
+    margin: 0.75em 0 0 0;
+    line-height: 1.5;
     font-family: var(--sc-serif-font);
 }
-h1{
-  font-size: 2em;
-  font-size: clamp(1.5em, 5vw, 2em);
-  font-weight: 300;
-  font-variant-caps: small-caps;
-  letter-spacing: var(--sc-caps-letter-spacing);
-  text-align: center;
-}
-h2{
-  font-size: 1.5em;
-  font-size: clamp(1.125em, 3.75vw, 1.5em);
-  font-weight: 400;
-  font-variant-caps: small-caps;
-  letter-spacing: var(--sc-caps-letter-spacing);
-  text-align: center;
-}
-h3{
-  font-size: 1.333em;
-  font-size: clamp(1em, 3.333vw, 1.333em);
-  font-weight: 400;
-  font-variant-caps: small-caps;
-  letter-spacing: var(--sc-caps-letter-spacing);
-  text-align: center;
-}
-h4{
-  font-size: clamp(1em, 5vw, 1.333em);;
-  font-weight: 600;
-}
-h5{
-  font-size: 1em;
-  font-weight: 600;
-}
-h6{
-  font-size: 1em;
-  font-weight: 400;
+blockquote {
+    margin: 0 2em;
 }
 
-  .evam  {
+/* currently supported by safari only */
+
+p,
+li {
+    hanging-punctuation: first last;
+}
+
+/* special text classes  */
+
+.evam {
     font-variant-caps: all-small-caps;
-  }
-
-  blockquote {
-    margin-left: 2em;
-    margin-right: 2em;
-  }
-
-   .endsection,
-  .end,
-  .endkanda  {
+}
+.namo {
+    text-align: center;
+    font-style: italic;
+}
+.speaker {
+    font-style: italic;
+    display: block;
+    margin-left: 1em;
+    height: 0;
+    color: var(--sc-secondary-text-color);
+}
+.endsection,
+.end,
+.endkanda {
     font-style: italic;
     text-align: center;
     color: var(--sc-secondary-text-color);
-  }
-
-  .endsutta {
+}
+.endsutta {
     font-weight: bold;
     text-align: center;
     color: var(--sc-secondary-text-color);
-  }
-    .endbook {
+}
+.endbook {
     font-weight: bold;
     text-align: center;
     color: var(--sc-secondary-text-color);
     text-transform: uppercase;
     letter-spacing: var(--sc-caps-letter-spacing);
-  }
-
-  .endvagga {
+}
+.endvagga {
     text-align: center;
     color: var(--sc-secondary-text-color);
     text-transform: uppercase;
     letter-spacing: var(--sc-caps-letter-spacing);
-  }
-
-  .uddana .text,
-  .uddanagatha .text
-   {
+}
+.uddana,
+.uddanagatha {
     color: var(--sc-secondary-text-color);
     font-size: var(--sc-skolar-font-size-s);
     font-weight: 400;
-  }
-
-  .uddana-intro .text {
+}
+.uddana-intro {
     font-weight: bold;
     color: var(--sc-secondary-text-color);
-  }
+}
 
+/* lookup */
+
+.spanFocused {
+    background-color: var(--sc-primary-color-light);
+    color: var(--sc-paper-tooltip-color);
+}
 
 /* Notes */
-
-  /* Anchor tooltips. */
-  article {
+/* Anchor tooltips. */
+article {
     position: relative;
-  }
-
-  .variant,
-  .comment,
-  a {
+}
+.variant,
+.comment,
+a {
     display: inline-block;
     font-family: var(--sc-sans-font);
     z-index: 10;
     box-sizing: border-box;
     color: var(--sc-secondary-text-color);
-  }
-
-  .variant,
-  .comment {
+}
+.variant,
+.comment {
     padding: var(--sc-size-sm) var(--sc-size-md);
     border-radius: var(--sc-size-sm);
     background-color: var(--sc-secondary-background-color);
     font-size: var(--sc-skolar-font-size-s);
     line-height: 1.3333;
-
-  }
-
-  .variant:hover,
-  .comment:hover{
+}
+.variant:hover,
+.comment:hover {
     cursor: help;
-  }
-
-.reference  a {
+}
+.reference a {
     color: var(--sc-secondary-text-color);
     text-decoration: none;
     white-space: nowrap;
-   font-feature-settings: "dnom";
-  font-weight: 500;
-  font-style: normal;
-  letter-spacing: -0.4px;
-  font-variant-caps: normal;
-  }
-  .reference  a:after{
+    font-feature-settings: "dnom";
+    font-weight: 500;
+    font-style: normal;
+    letter-spacing: -0.4px;
+    font-variant-caps: normal;
+}
+.reference a:after {
     content: " ";
     white-space: pre;
-  }
-  header .reference a,
-  h2 .reference a,
-  h3 .reference a,
-  h4 .reference a,
-  h5 .reference a,
-  h6 .reference a{
+}
+header .reference a,
+h2 .reference a,
+h3 .reference a,
+h4 .reference a,
+h5 .reference a,
+h6 .reference a {
     display: none
-  }
-
-  .pts:before{
+}
+.pts:before {
     content: "pts";
     font-variant-caps: all-small-caps;
-     font-feature-settings: "ordn";
-  }
-
-
-
-  /* Set shared styles for the tooltip, but don't show it. Specify classes, avoid unattributed attribute selector for performance.*/
-
-  .comment[data-tooltip]::after,
-  .variant[data-tooltip]::after {
+    font-feature-settings: "ordn";
+}
+/* Set shared styles for the tooltip, but don't show it. Specify classes, avoid unattributed attribute selector for performance.*/
+.comment[data-tooltip]::after,
+.variant[data-tooltip]::after {
     content: attr(data-tooltip);
     display: none;
     position: absolute;
@@ -234,28 +244,17 @@ h6{
     box-sizing: border-box;
     background-color: var(--sc-secondary-background-color);
     color: var(--sc-secondary-text-color);
-  }
-
-  .comment[data-tooltip]::after {
+}
+.comment[data-tooltip]::after {
     border-style: solid;
     border-width: 1px 1px 1px 4px;
     border-color: var(--sc-primary-accent-color);
-  }
-
-  .variant[data-tooltip]::after {
+}
+.variant[data-tooltip]::after {
     border-style: solid;
     border-width: 1px 1px 1px 4px;
     border-color: var(--sc-secondary-accent-color);
-  }
-
-  .spanFocused {
-    background-color: var(--sc-primary-color-light);
-    color: var(--sc-paper-tooltip-color);
-  }
-
-  span, p, li {
-    hanging-punctuation: first last;
-  }
+}
 
 `;
 
