2e9730f81d47955259e0b619ab382aa8804df20f
sujato <sujato@gmail.com>
2020-11-01 22:34:53 +1100

    redo help for bottom sheet

--- client/elements/addons/sc-toasts.js
+++ client/elements/addons/sc-toasts.js
@@ -9,11 +9,16 @@ class SCToasts extends LitElement {
         display: block;
         position: absolute;
         z-index: 9999;
-        --mdc-typography-body2-font-size: calc(19px * var(--sc-skolar-font-scale));
+        --mdc-typography-body2-font-size: calc(19px * var(--sc-skolar-font-scale));        
+      }
+
+      .mdc-snackbar__label{
+        
       }
 
       .toast {
         text-align: center;
+        font-family: var(--sc-serif-font);
       }
 
       .success-toast {
