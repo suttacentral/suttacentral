2a600e91df68b331b8bf7944554240aee320b68f
sujato <sujato@gmail.com>
2020-12-01 19:48:41 +1100

    merge master into sujato-42

---
+++ client/elements/addons/sc-linear-progress.js
@@ -0,0 +1,77 @@
+import { LitElement, html, css } from 'lit-element';
+
+class SCLinearProgress extends LitElement {
+  static get styles() {
+    return css`
+.linear-progress,
+.linear-progress:before
+{
+    width: 100%;
+    margin: -2px 0 0 0;
+}
+
+.linear-progress
+{
+    display: flex;
+}
+
+.linear-progress:before
+{
+    height: 4px;
+
+    content: '';
+    animation: running-progress 2s cubic-bezier(0.8, 0.6, 0.8, 0.9) infinite;
+
+    background-image: linear-gradient(
+    to right,
+    rgba(0,0,0,0),
+    var(--sc-primary-color-light),
+    rgba(0,0,0,0)
+    );
+}
+
+@keyframes running-progress
+{
+    0%
+    {
+        margin-right: 100%;
+        margin-left: 0;
+    }
+
+    50%
+    {
+        margin-right: 0;
+        margin-left: 25%;
+    }
+
+    100%
+    {
+        margin-right: 0;
+        margin-left: 100%;
+    }
+}
+    `;
+  }
+
+  static get properties() {
+    return {
+      active: { type: Boolean }
+    };
+  }
+
+  constructor() {
+    super();
+    this.active = true;
+  }
+
+  render() {
+    return this.active ? html`
+  <div class="linear-progress"></div>
+    ` : ''
+  }
+}
+
+customElements.define('sc-linear-progress', SCLinearProgress);
+
+
+
