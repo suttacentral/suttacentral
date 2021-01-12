f78d376cd9a208b0f633ab584db4372cfc9be704
sujato <sujato@gmail.com>
2021-01-12 17:16:55 +1100

    remove styles, remove list class

--- client/elements/addons/sc-top-sheet-toc.js
+++ client/elements/addons/sc-top-sheet-toc.js
@@ -2,45 +2,6 @@ import {html, css} from 'lit-element';
 import SCTopSheetCommon from "./sc-top-sheet-common";
 import { typographyCommonStyles } from '../styles/sc-typography-common-styles.js';
 
-const styles = css`
-.contents {
-    margin-top: 2em;
-    padding: 0 var(--sc-size-md);
-    max-width: 720px;
-    display: block;
-    border-radius: 2px;
-    border-left: 4px solid #F6C200;
-    text-align: left;
-}
-
-li {
-  font-family: var(--sc-paper-font-body_-_font-family); -webkit-font-smoothing: var(--sc-paper-font-body_-_-webkit-font-smoothing); font-feature-settings: var(--sc-paper-font-body_-_font-feature-settings); font-size: var(--sc-paper-font-body_-_font-size); font-weight: var(--sc-paper-font-body_-_font-weight); line-height: var(--sc-paper-font-body_-_line-height);
-    margin: 0.5em 0;
-    padding: var(--sc-size-xs) 0;
-}
-
-ol {
-  margin: 1em 0 0 0;
-}
-
-.unordered-ol{
-  margin: 1em 0 0 0;
-  list-style: none;
-}
-
-a {
-  color: var(--sc-inline-link_-_color); text-decoration: var(--sc-inline-link_-_text-decoration); text-decoration-color: var(--sc-inline-link_-_text-decoration-color); text-decoration-skip-ink: var(--sc-inline-link_-_text-decoration-skip-ink);
-    text-decoration: none;
-}
-
-a:hover {
-  color: var(--sc-inline-link-hover_-_color);
-}
-
-a:visited {
-  text-decoration-color: var(--sc-inline-link-visited_-_text-decoration-color);
-}`;
-
 class SCTopSheetToC extends SCTopSheetCommon {
     static get styles() {
         return [
@@ -89,7 +50,7 @@ class SCTopSheetToC extends SCTopSheetCommon {
     <section>
     <h2>Table of Contents</h2>
       <nav class="contents">
-        <ol class=${this.disableToCListStyle ? "unordered-ol" : ""}>
+        <ol>
           ${this.items ? this.items.map(item => html`<li><a @click=${this._hideMenu} href="${'#' + item.link}">${item.name}</a></li>`) : ''}
         </ol>
       </nav>
