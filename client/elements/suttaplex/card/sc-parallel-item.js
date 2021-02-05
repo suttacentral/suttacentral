ee66f9eb989edf630f8be759dc61c402ace83a38
sujato <sujato@gmail.com>
2021-02-06 07:46:11 +1100

    remove polymer dom-if

--- client/elements/suttaplex/card/sc-parallel-item.js
+++ client/elements/suttaplex/card/sc-parallel-item.js
@@ -1,16 +1,15 @@
 import { html, LitElement, svg } from 'lit-element';
-import '@polymer/polymer/lib/elements/dom-if.js';
 import { icon } from '../../../img/sc-icon';
 import {
   getParagraphRange,
   transformId,
   pickVolPage,
-  hasTwoPTSEditions
+  hasTwoPTSEditions,
 } from '../../../utils/suttaplex';
 import { LitLocalized } from '../../addons/localization-mixin';
 import { parallelItemCss } from './sc-suttaplex-css';
 
-const stopPropagation = (e) => e.stopPropagation();
+const stopPropagation = e => e.stopPropagation();
 
 class SCParallelItem extends LitLocalized(LitElement) {
   static get properties() {
@@ -19,23 +18,23 @@ class SCParallelItem extends LitLocalized(LitElement) {
       remark: String,
       rootLangMappings: Object,
       localizedStringsPath: String,
-      expansionData: Array
-    }
+      expansionData: Array,
+    };
   }
 
   constructor() {
     super();
     this.localizedStringsPath = '/localization/elements/sc-parallel-item';
     this.rootLangMappings = {
-      'pli': 'Pali',
-      'lzh': 'Chinese',
-      'san': 'Sanskrit',
-      'xct': 'Tibetan',
-      'pra': 'Prakrit',
-      'pgd': 'Gandhari',
-      'uig': 'Uighur',
-      'xto': 'TocharianA',
-      'kho': 'Khotanese'
+      pli: 'Pali',
+      lzh: 'Chinese',
+      san: 'Sanskrit',
+      xct: 'Tibetan',
+      pra: 'Prakrit',
+      pgd: 'Gandhari',
+      uig: 'Uighur',
+      xto: 'TocharianA',
+      kho: 'Khotanese',
     };
   }
 
@@ -53,7 +52,11 @@ class SCParallelItem extends LitLocalized(LitElement) {
   }
 
   get headingTitle() {
-    if (!this.parallelItem || this.parallelItem.translated_title || this.parallelItem.original_title) {
+    if (
+      !this.parallelItem ||
+      this.parallelItem.translated_title ||
+      this.parallelItem.original_title
+    ) {
       return '';
     } else {
       return this.acronymTitle;
@@ -68,8 +71,10 @@ class SCParallelItem extends LitLocalized(LitElement) {
     const translation = this.parallelItem.translations[0];
 
     if (translation) {
-      return `/${this.parallelItem.uid}/${translation.lang}` +
-        `/${translation.author_uid}${getParagraphRange(this.parallelItem.to, true)}`;
+      return (
+        `/${this.parallelItem.uid}/${translation.lang}` +
+        `/${translation.author_uid}${getParagraphRange(this.parallelItem.to, true)}`
+      );
     } else {
       return '';
     }
@@ -85,7 +90,7 @@ class SCParallelItem extends LitLocalized(LitElement) {
       const altNumber = this.parallelItem.acronym.split('//')[1];
       if (altNumber) {
         let book = '';
-        (altNumber[0] === 'T') ? book = 'Taishō' : book = 'PTS';
+        altNumber[0] === 'T' ? (book = 'Taishō') : (book = 'PTS');
         scAcronymTitle += `\n${this.localize('alternateText', 'book', book)} ${altNumber}`;
       }
     }
@@ -97,7 +102,10 @@ class SCParallelItem extends LitLocalized(LitElement) {
     if (this.parallelItem.acronym) {
       scAcronym = this.parallelItem.acronym.split('//')[0];
       const rootId = this.parallelItem.to.replace('~', '');
-      const idPart = getParagraphRange(rootId).replace('--', '–').replace('#', ': ').replace(/[a-z]+/g,'');
+      const idPart = getParagraphRange(rootId)
+        .replace('--', '–')
+        .replace('#', ': ')
+        .replace(/[a-z]+/g, '');
       return `${scAcronym}${idPart}`;
     } else {
       scAcronym = transformId(this.parallelItem.to, this.expansionData, 0);
@@ -110,59 +118,76 @@ class SCParallelItem extends LitLocalized(LitElement) {
   }
 
   get volPageTitle() {
-    return hasTwoPTSEditions(this.parallelItem.volpages) ?
-      this.localize('volumeAndPagePTS1', this.parallelItem.volpages) :
-      this.localize('volumeAndPage');
+    return hasTwoPTSEditions(this.parallelItem.volpages)
+      ? this.localize('volumeAndPagePTS1', this.parallelItem.volpages)
+      : this.localize('volumeAndPage');
   }
 
   render() {
     return html`
-    ${parallelItemCss}
+      ${parallelItemCss}
 
-    <a href="${this.parallelUrl}" class="${this.parallelUrl ? '' : 'disabled'}">
-      <div class="parallel-item">
-        <div class="parallel-item-main-info-container">
-          <div class="parallel-item-title" title="${this.headingTitle}">
-            ${this.heading}
-          </div>
-  
-          <div class="parallel-item-nerdy-row">
-              ${this.parallelItem.translated_title && this.parallelItem.original_title ? html`
-                <div title="${this.localize('originalTitle')}" class="nerdy-row-element">
-                  ${this.titleWithoutSuttaText}
-                </div>
-              ` : ''}
+      <a href="${this.parallelUrl}" class="${this.parallelUrl ? '' : 'disabled'}">
+        <div class="parallel-item">
+          <div class="parallel-item-main-info-container">
+            <div class="parallel-item-title" title="${this.headingTitle}">${this.heading}</div>
 
-              ${this.parallelItem.translated_title || this.parallelItem.original_title ? html`
-                <div class="nerdy-row-element" title="${this.acronymTitle}">
-                  ${this.acronymOrUid}
-                </div>
-              ` : ''}
-
-              ${this.volpagesAvailable ? html`
-                <div class="nerdy-row-element" @click=${stopPropagation} @tap="${stopPropagation}" @mousedown="${stopPropagation}">
-                  ${this.parallelItem.biblio && html`
-                    <details>
-                      <summary>
-                        ${icon.book}
-                        <span class="vol-page" title="${this.volPageTitle}">
-                          ${this.volPage}
-                        </span>
-                      </summary>
-                      <p class="parallel-item-biblio-info" .innerHTML="${this.parallelItem.biblio}"></p>
-                    </details>
-                  `}
-                  ${!this.parallelItem.biblio ? html`
-                    <span class="book scrollable-dialog" title="${this.localize('volumeAndPage')}">
-                      ${icon.book}
-                    </span>
-                    <span class="vol-page" title="${this.volPageTitle}">
-                      ${this.volPage}
-                    </span>
-                  ` : ''}
-                </div>
-              ` : ''}
-              ${this.remark && html`
+            <div class="parallel-item-nerdy-row">
+              ${this.parallelItem.translated_title && this.parallelItem.original_title
+                ? html`
+                    <div title="${this.localize('originalTitle')}" class="nerdy-row-element">
+                      ${this.titleWithoutSuttaText}
+                    </div>
+                  `
+                : ''}
+              ${this.parallelItem.translated_title || this.parallelItem.original_title
+                ? html`
+                    <div class="nerdy-row-element" title="${this.acronymTitle}">
+                      ${this.acronymOrUid}
+                    </div>
+                  `
+                : ''}
+              ${this.volpagesAvailable
+                ? html`
+                    <div
+                      class="nerdy-row-element"
+                      @click=${stopPropagation}
+                      @tap="${stopPropagation}"
+                      @mousedown="${stopPropagation}"
+                    >
+                      ${this.parallelItem.biblio &&
+                      html`
+                        <details>
+                          <summary>
+                            ${icon.book}
+                            <span class="vol-page" title="${this.volPageTitle}">
+                              ${this.volPage}
+                            </span>
+                          </summary>
+                          <p
+                            class="parallel-item-biblio-info"
+                            .innerHTML="${this.parallelItem.biblio}"
+                          ></p>
+                        </details>
+                      `}
+                      ${!this.parallelItem.biblio
+                        ? html`
+                            <span
+                              class="book scrollable-dialog"
+                              title="${this.localize('volumeAndPage')}"
+                            >
+                              ${icon.book}
+                            </span>
+                            <span class="vol-page" title="${this.volPageTitle}">
+                              ${this.volPage}
+                            </span>
+                          `
+                        : ''}
+                    </div>
+                  `
+                : ''}
+              ${this.remark &&
+              html`
                 <div "nerdy-row-element" @click=${stopPropagation} @tap="${stopPropagation}" @mousedown="${stopPropagation}">
                   <details>
                     <summary>
@@ -173,7 +198,8 @@ class SCParallelItem extends LitLocalized(LitElement) {
                 </div>
               `}
             </div>
-            ${this.parallelItem.note && html`
+            ${this.parallelItem.note &&
+            html`
               <div "nerdy-row-element" @click=${stopPropagation} @tap="${stopPropagation}" @mousedown="${stopPropagation}">
                 <details>
                   <summary>
@@ -184,8 +210,9 @@ class SCParallelItem extends LitLocalized(LitElement) {
               </div>
             `}
           </div>
-      </div>
-    </a>`;
+        </div>
+      </a>
+    `;
   }
 }
 
