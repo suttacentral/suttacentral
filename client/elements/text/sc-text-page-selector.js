6c670edd1f8c4c637355b3cbcf71a89afd36ce4f
Hongda <paccakkha@yahoo.com>
2021-02-16 22:25:33 +0800

    Update sc-text-page-selector.js

--- client/elements/text/sc-text-page-selector.js
+++ client/elements/text/sc-text-page-selector.js
@@ -323,7 +323,7 @@ class SCTextPageSelector extends LitLocalized(LitElement) {
   async _genNavDetail() {
     const suttaFullPath = await this._fetchSuttaFullPath();
     const navArray = store.getState().navigationArray;
-    if (suttaFullPath && navArray[1].type !== 'pitaka') {
+    if (suttaFullPath && navArray[1] && navArray[1].type !== 'pitaka') {
       parseURL(suttaFullPath.full_path, navArray);
       this.actions.setNavigation(navArray);
     }
