30df7b660cfa95cccb2b24f6bb0cf4b1c2dbbfc8
Alexander Voloshchenko <voloshchenkoalexander@gmail.com>
2021-02-22 21:19:23 +0200

    Hyphenation done
    - added hyphenators classes
    - removed todos

--- server/server/common/hyphenator.py
+++ server/server/common/hyphenator.py
@@ -10,43 +10,55 @@ import regex
 
 from data_loader.util import json_load
 
-# todo: create classes for each hyphenator with their segments files
-# todo: one for Pali lang and the second one for Sanskrit
 FOLDER = Path(__file__).parent
-segments = set(json_load(FOLDER / 'hyphenator_san.json')['segments'])
-
-cons = '(?:br|[kgcjtṭdḍbp]h|[kgcjtṭdḍp](?!h)|[mnyrlvshṅṇṃṁñḷ]|b(?![rh]))'
-vowel_chars = 'aioueāīū'
-vowel_pattern = '[' + vowel_chars.lower() + ']'
-vowel_antipattern = '[^' + vowel_chars.lower() + '-]'
-
-segments_revoweled = [regex.sub(vowel_pattern + '$', vowel_pattern, segment, flags=regex.I) for segment in
-                      sorted(segments, key=len, reverse=True)]
-
-segment_rex = regex.compile('({})'.format('|'.join(segments_revoweled)), flags=regex.I)
-
-alpha_rex = regex.compile(r'\p{alpha}+')
 
 
-def fix_hyphens(word: str) -> str:
-    for i in range(0, 2):
-        word = regex.sub(r'-({})({})'.format(cons, cons), r'\1-\2', word, flags=regex.I)
-        word = regex.sub(r'([kgcjḍṭdtpb])-(h{})'.format(vowel_pattern), r'\1\2-', word, flags=regex.I)
-    word = regex.sub(r'^(\p{alpha}{0,3})-', r'\1', word)
-    word = regex.sub(r'-(\p{alpha}{0,3})$', r'\1', word)
-    return word
+class Hyphenator:
+    _HYPHENATION_MAX_WORD_LEN = 20
 
+    def __init__(self, segments_file: Path):
+        vowel_chars = 'aioueāīū'
 
-def hyphenate(word: str, max_length: int) -> str:
-    if len(word) <= max_length or '\xad' in word:
+        self.segments = set(json_load(segments_file)['segments'])
+        self.cons = '(?:br|[kgcjtṭdḍbp]h|[kgcjtṭdḍp](?!h)|[mnyrlvshṅṇṃṁñḷ]|b(?![rh]))'
+        self.vowel_pattern = '[' + vowel_chars.lower() + ']'
+
+        segments_revoweled = [regex.sub(self.vowel_pattern + '$', self.vowel_pattern, segment, flags=regex.I)
+                              for segment in sorted(self.segments, key=len, reverse=True)]
+
+        self.segment_rex = regex.compile('({})'.format('|'.join(segments_revoweled)), flags=regex.I)
+        self.alpha_rex = regex.compile(r'\p{alpha}+')
+
+    def _fix_hyphens(self, word: str) -> str:
+        for i in range(0, 2):
+            word = regex.sub(r'-({})({})'.format(self.cons, self.cons), r'\1-\2', word, flags=regex.I)
+            word = regex.sub(r'([kgcjḍṭdtpb])-(h{})'.format(self.vowel_pattern), r'\1\2-', word, flags=regex.I)
+        word = regex.sub(r'^(\p{alpha}{0,3})-', r'\1', word)
+        word = regex.sub(r'-(\p{alpha}{0,3})$', r'\1', word)
         return word
 
-    word = segment_rex.sub(r'-\1-', word)
-    word = word.replace('--', '-')
-    word = word.strip('-')
-    word = fix_hyphens(word)
+    def hyphenate(self, word: str) -> str:
+        if len(word) <= self._HYPHENATION_MAX_WORD_LEN or '\xad' in word:
+            return word
 
-    for segment in alpha_rex.findall(word):
-        if len(segment) > max_length:
-            logging.warning(f'Segment too long: {segment}')
-    return word.replace('-', '\xad')
+        word = self.segment_rex.sub(r'-\1-', word)
+        word = word.replace('--', '-')
+        word = word.strip('-')
+        word = self._fix_hyphens(word)
+
+        for segment in self.alpha_rex.findall(word):
+            if len(segment) > self._HYPHENATION_MAX_WORD_LEN:
+                logging.warning(f'Segment too long: {segment}')
+        return word.replace('-', '\xad')
+
+
+class PaliHyphenator(Hyphenator):
+
+    def __init__(self):
+        super().__init__(FOLDER / 'hyphenator_pli.json', )
+
+
+class SanskritHyphenator(Hyphenator):
+
+    def __init__(self):
+        super().__init__(FOLDER / 'hyphenator_san.json')
--- server/server/data_loader/hyphenation.py
+++ server/server/data_loader/hyphenation.py
@@ -6,20 +6,18 @@ from bs4.element import Tag, NavigableString
 from tqdm import tqdm
 from typing import Dict, Union
 
-from common.hyphenator import hyphenate
+from common.hyphenator import Hyphenator, PaliHyphenator, SanskritHyphenator
 from common.queries import ALL_TEXTS_BY_LANGUAGES
 from data_loader.util import json_load, json_save
 
-_HYPHENATION_MAX_WORD_LEN = 20
 
-
-def _hyphenate_modern_text(text_file: Path) -> None:
+def _hyphenate_modern_text(text_file: Path, hyphenator: Hyphenator) -> None:
     has_changes = False
     file_content: Dict[str, str] = json_load(text_file)
     for key, string in file_content.items():
         if not string:
             continue
-        hyphenated_words = (hyphenate(word, _HYPHENATION_MAX_WORD_LEN) for word in string.split())
+        hyphenated_words = (hyphenator.hyphenate(word) for word in string.split())
         hyphenated_string = ' '.join(hyphenated_words)
         if string[-1] == ' ':
             hyphenated_string = hyphenated_string + ' '
@@ -31,7 +29,7 @@ def _hyphenate_modern_text(text_file: Path) -> None:
         json_save(file_content, text_file)
 
 
-def _hyphenate_legacy_text(text_file: Path) -> None:
+def _hyphenate_legacy_text(text_file: Path, hyphenator: Hyphenator) -> None:
     has_changes = False
     with open(text_file) as fp:
         soup = BeautifulSoup(fp, 'lxml', from_encoding='utf-8')
@@ -40,7 +38,7 @@ def _hyphenate_legacy_text(text_file: Path) -> None:
         if not suitable_node(child):
             continue
         original_string = child.string.strip()
-        hyphenated_words = (hyphenate(word, _HYPHENATION_MAX_WORD_LEN) for word in original_string.split())
+        hyphenated_words = (hyphenator.hyphenate(word) for word in original_string.split())
         hyphenated_string = ' '.join(hyphenated_words)
         if original_string != hyphenated_string:
             has_changes = True
@@ -56,13 +54,13 @@ def suitable_node(element: Union[Tag, NavigableString]) -> bool:
 
 
 def hyphenate_texts(db: Database) -> None:
-    # TODO: uncomment when html texts parsing will be done
-    # languages = ('pli', 'san')
-    languages = ('san',)
+    languages = ('pli', 'san')
     texts = db.aql.execute(ALL_TEXTS_BY_LANGUAGES, bind_vars={'languages': languages}, ttl=1200)
+    pali_hyphenator = PaliHyphenator()
+    san_hyphenator = SanskritHyphenator()
     for text in tqdm(texts):
         text_file = Path(text['file_path'])
         if text_file.suffix == '.json':
-            _hyphenate_modern_text(text_file)
+            _hyphenate_modern_text(text_file, pali_hyphenator)
         else:
-            _hyphenate_legacy_text(text_file)
+            _hyphenate_legacy_text(text_file, san_hyphenator)
