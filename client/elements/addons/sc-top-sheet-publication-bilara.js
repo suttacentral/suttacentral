d108ff77483c24c7ef3c13bcd2d0dae8582cef05
sujato <sujato@gmail.com>
2021-03-05 20:42:29 +1100

    style publication

--- client/elements/addons/sc-top-sheet-publication-bilara.js
+++ client/elements/addons/sc-top-sheet-publication-bilara.js
@@ -83,7 +83,78 @@ class SCTopSheetPublicationBilara extends SCTopSheetCommon {
   }
 
   static get styles() {
-    return [super.styles, typographyCommonStyles];
+    return [
+      super.styles,
+      typographyCommonStyles,
+      css`
+        section > section {
+          font-family: var(--sc-sans-font);
+
+          margin: 0;
+          padding: 0;
+        }
+
+        dl {
+          margin: 1em 0;
+          padding: 0 1rem 1rem 1rem;
+
+          border: var(--sc-border);
+          border-radius: 8px;
+        }
+
+        .main-details,
+        .edition {
+          display: grid;
+
+          grid-template-columns: 1fr 3fr;
+          align-items: baseline;
+          gap: 0 1em;
+        }
+
+        dt {
+          font-weight: 600;
+          font-style: italic;
+
+          color: var(--sc-secondary-text-color);
+        }
+
+        dd {
+          margin-left: 0;
+        }
+
+        .number-of_volumes,
+        .text-uid {
+          display: none;
+        }
+
+        .license {
+          padding: 0 1rem 1rem 1rem;
+
+          color: var(--sc-secondary-text-color);
+          border: var(--sc-border);
+          border-radius: 8px;
+          background: var(--sc-tertiary-background-color);
+        }
+
+        .license-type {
+          font-weight: 700;
+        }
+
+        dd a {
+          overflow-wrap: break-word;
+        }
+
+        a img {
+          display: block;
+
+          margin: 0 0 0.75em 0;
+        }
+
+        [property='vcard:Country'] {
+          font-weight: 700;
+        }
+      `,
+    ];
   }
 
   constructor() {
@@ -150,91 +221,107 @@ class SCTopSheetPublicationBilara extends SCTopSheetCommon {
   render() {
     if (this.isPublished)
       return html`
-        <h2>About this text</h2>
-        <section class="text-metadata" about="${this.sourceURL}">
-          <dl class="main-details">
-            <dt class="translation-title">Translation title</dt>
-            <dd class="translation-title" property="dc:title">${this.translationTitle}</dd>
-            <dt class="translation-subtitle">Translation subtitle</dt>
-            <dd class="translation-subtitle" property="dc:title">${this.translationSubtitle}</dd>
-            <dt class="translation-language">Translation language</dt>
-            <dd class="translation-language" property="dc:language">${this.translationLanguage}</dd>
-            <dt class="root-title">Root title</dt>
-            <dd class="root-title" property="dc:title">${this.rootTitle}</dd>
-            <dt class="root-language">Root language</dt>
-            <dd class="root-language">${this.rootLanguage}</dd>
-            <dt class="author-name">Translator</dt>
-            <dd class="author-name" property="dc:creator">${this.authorName}</dd>
-          </dl>
-          <dl class="descriptive-details">
-            <dt class="translation-description">Translation description</dt>
-            <dd class="translation-description" property="dc:description">
-              ${this.translationDescription}
-            </dd>
-            <dt class="translation-process">Translation process</dt>
-            <dd class="translation-process" property="dc:description">
-              ${this.translationProcess}
-            </dd>
-          </dl>
-          <dl class="metadata-details">
-            <dt class="text-uid">Text identifier (UID)</dt>
-            <dd class="text-uid" property="dc:identifier">${this.textUID}</dd>
-            <dt class="source-url">Source</dt>
-            <dd class="source-url">
-              <a href="${this.sourceURL}" target="_blank">${this.sourceURL}</a>
-            </dd>
-            <dt class="publication-status">Publication status</dt>
-            <dd class="publication-status">${this.publicationStatus}</dd>
-            <dt class="publication-number">SuttaCentral publication number</dt>
-            <dd class="publication-number" property="dc:identifier">${this.publicationNumber}</dd>
-          </dl>
-          <dl class="edition">
-            <dt class="edition-number">Edition</dt>
-            <dd class="edition-number">${this.editionNumber}</dd>
-            <dt class="publication-date">Publication date</dt>
-            <dd class="publication-date" property="dc:date">${this.publicationDate}</dd>
-            <dt class="publisher">Publisher</dt>
-            <dd class="publisher" property="dc:publisher">${this.publisher}</dd>
-            <dt class="edition-url">URL</dt>
-            <dd class="edition-url">${this.editionURL}</dd>
-            <dt class="publication-type">Publication type</dt>
-            <dd class="publication-type" property="dc:format">${this.publicationType}</dd>
-            <dt class="number-of_volumes">Number of volumes</dt>
-            <dd class="number-of_volumes">${this.numberOfVolumes}</dd>
-          </dl>
-        </section>
-        <section class="license">
-          <p class="license-type" property="dc:rights">
-            ${this.licenseType}
-            <span class="license-abbreviation">${this.licenseAbbreviation}</span>
-          </p>
-          <p class="creative-commons">
-            <a rel="license" href="${this.license_url}">
-              <img
-                src="https://i.creativecommons.org/p/zero/1.0/88x31.png"
-                style="border-style: none;"
-                alt="CC0"
-              />
-            </a>
-            <br />
-            To the extent possible under law,
-            <a rel="dct:publisher" href="https://suttacentral.net/">
-              <span property="dct:title">${this.authorName}</span>
-            </a>
-            has waived all copyright and related or neighboring rights to
-            <span property="dct:title">${this.translationTitle}</span>
-            . This work is published from:
-            <span
-              property="vcard:Country"
-              datatype="dct:ISO3166"
-              content="AU"
-              about="https://suttacentral.net/licensing"
-            >
-              Australia
-            </span>
-            .
-          </p>
-          <p class="license-statement">${this.licenseStatement}</p>
+        <section>
+          <h2>Publication details</h2>
+
+          <section class="text-metadata" about="${this.sourceURL}">
+            <p>
+              This text is included in
+              <cite>${this.translationTitle}</cite>
+              by
+              <span>${this.authorName}</span>
+              .
+            </p>
+            <dl class="main-details">
+              <dt class="translation-title">Translation title</dt>
+              <dd class="translation-title" property="dc:title">${this.translationTitle}</dd>
+              <dt class="translation-subtitle">Translation subtitle</dt>
+              <dd class="translation-subtitle" property="dc:title">${this.translationSubtitle}</dd>
+              <dt class="translation-language">Translation language</dt>
+              <dd class="translation-language" property="dc:language">
+                ${this.translationLanguage}
+              </dd>
+              <dt class="root-title">Root title</dt>
+              <dd class="root-title" property="dc:title">${this.rootTitle}</dd>
+              <dt class="root-language">Root language</dt>
+              <dd class="root-language">${this.rootLanguage}</dd>
+              <dt class="author-name">Translator</dt>
+              <dd class="author-name" property="dc:creator">${this.authorName}</dd>
+            </dl>
+            <dl class="descriptive-details">
+              <dt class="translation-description">Translation description</dt>
+              <dd class="translation-description" property="dc:description">
+                ${this.translationDescription}
+              </dd>
+              <dt class="translation-process">Translation process</dt>
+              <dd class="translation-process" property="dc:description">
+                ${this.translationProcess}
+              </dd>
+              <dt class="publication-status">Publication status</dt>
+              <dd class="publication-status">${this.publicationStatus}</dd>
+            </dl>
+            <dl class="edition">
+              <dt class="edition-number">Edition</dt>
+              <dd class="edition-number">${this.editionNumber}</dd>
+              <dt class="publication-date">Publication date</dt>
+              <dd class="publication-date" property="dc:date">${this.publicationDate}</dd>
+              <dt class="publisher">Publisher</dt>
+              <dd class="publisher" property="dc:publisher">${this.publisher}</dd>
+              <dt class="publication-type">Publication type</dt>
+              <dd class="publication-type" property="dc:format">${this.publicationType}</dd>
+              <dt class="number-of_volumes">Number of volumes</dt>
+              <dd class="number-of_volumes">${this.numberOfVolumes}</dd>
+            </dl>
+            <dl class="metadata-details">
+              <dt class="text-uid">Text identifier (UID)</dt>
+              <dd class="text-uid" property="dc:identifier">${this.textUID}</dd>
+              <dt class="edition-url">URL</dt>
+              <dd class="edition-url">
+                <a href="${this.editionURL}" target="_blank" rel="noopener">${this.editionURL}</a>
+              </dd>
+              <dt class="source-url">Source</dt>
+              <dd class="source-url">
+                <a href="${this.sourceURL}" target="_blank" rel="noopener">${this.sourceURL}</a>
+              </dd>
+              <dt class="publication-number">SuttaCentral publication number</dt>
+              <dd class="publication-number" property="dc:identifier">${this.publicationNumber}</dd>
+            </dl>
+          </section>
+          <section class="license">
+            <h3>License</h3>
+            <p class="license-type" property="dc:rights">
+              ${this.licenseType} (
+              <span class="license-abbreviation">${this.licenseAbbreviation}</span>
+              )
+            </p>
+            <p class="creative-commons">
+              <a rel="license" href="${this.license_url}">
+                <img
+                  src="https://i.creativecommons.org/p/zero/1.0/88x31.png"
+                  style="border-style: none;"
+                  alt="CC0"
+                />
+              </a>
+              To the extent possible under law,
+              <a rel="dct:publisher" href="https://suttacentral.net/">
+                <span property="dct:title">${this.authorName}</span>
+              </a>
+              has waived all copyright and related or neighboring rights to
+              <cite property="dct:title">${this.translationTitle}</cite>
+              . This work is published from
+              <span
+                property="vcard:Country"
+                datatype="dct:ISO3166"
+                content="AU"
+                about="https://suttacentral.net/licensing"
+              >
+                Australia
+              </span>
+              .
+            </p>
+            <h3>About this license</h3>
+            <p class="license-statement">${this.licenseStatement}</p>
+          </section>
         </section>
       `;
   }
