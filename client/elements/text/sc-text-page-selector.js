684e9d2656d21463f77ede09a6185a163509748e
iHongda <hd2935@qq.com>
2021-03-07 18:45:21 +0800

    Refine the error prompt.

--- client/elements/text/sc-text-page-selector.js
+++ client/elements/text/sc-text-page-selector.js
@@ -338,15 +338,6 @@ class SCTextPageSelector extends LitLocalized(LitElement) {
         type: 'data-load-error',
       };
     }
-    if (
-      this.responseData.segmented &&
-      this.responseData.translation.lang !== 'pli' &&
-      !this.bilaraTranslatedSutta
-    ) {
-      this.lastError = {
-        type: 'translation-text-load-error',
-      };
-    }
   }
 
   setProperties() {
@@ -428,6 +419,15 @@ class SCTextPageSelector extends LitLocalized(LitElement) {
       this.suttaReference = bilaraData.reference_text;
       this.suttaComment = bilaraData.comment_text;
       this.suttaVariant = bilaraData.variant_text;
+      if (
+        this.responseData.segmented &&
+        this.responseData.translation.lang !== 'pli' &&
+        !this.bilaraTranslatedSutta
+      ) {
+        this.lastError = {
+          type: 'translation-text-load-error',
+        };
+      }
     } catch (error) {
       this.lastError = error;
     }
