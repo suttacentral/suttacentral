1ce99ea8f88584bcac3443b436489572d8443d84
ihongda <hd2935@qq.com>
2021-05-24 10:07:10 +0800

    Add `babel-loader` to `webpack` to support new JS features.

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -16,6 +16,9 @@ module.exports = {
   resolve: {
     mainFields: ['es2015', 'module', 'main'],
   },
+  experiments: {
+    topLevelAwait: true,
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html',
@@ -65,6 +68,27 @@ module.exports = {
   ],
   module: {
     rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+            plugins: [
+              ['@babel/plugin-proposal-class-static-block'],
+              ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
+              ['@babel/plugin-proposal-class-properties', { loose: true }],
+              ['@babel/plugin-transform-runtime'],
+              ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
+              ['@babel/plugin-proposal-function-bind'],
+              ['@babel/plugin-proposal-do-expressions'],
+              ['@babel/plugin-proposal-throw-expressions'],
+              ['@babel/plugin-proposal-async-do-expressions'],
+            ],
+          },
+        },
+      },
       {
         test: /\.css$/,
         use: [
