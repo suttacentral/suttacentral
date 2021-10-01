a392216e9c23b78fbc10349b5fc9a664464d4b45
ihongda <hd2935@qq.com>
2021-10-01 21:06:26 +0800

    Use another way to display orphan data.

--- server/server/common/queries.py
+++ server/server/common/queries.py
@@ -514,7 +514,6 @@ FOR v, e, p IN 0..6 OUTBOUND CONCAT('super_nav_details/', @uid) super_nav_detail
 
 PARALLELS = '''
 FOR v, e, p IN OUTBOUND CONCAT('super_nav_details/', @uid) relationship
-    FILTER v.uid != null
     LET target = DOCUMENT(e._to)
 
     LET legacy_translations = (
@@ -612,7 +611,7 @@ FOR v, e, p IN OUTBOUND CONCAT('super_nav_details/', @uid) relationship
             volpages: volpages,
             alt_volpages: alt_volpages,
             acronym: v.acronym,
-            uid: v.uid,
+            uid: v.uid ? v.uid : 'orphan',
             root_lang: v.root_lang,
             original_title: original_titles,
             translated_title: translated_titles,
