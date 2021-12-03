f9dffa0e5d9f989aff5e08fcb58f2d9b66f9a8bd
sujato <sujato@gmail.com>
2021-12-04 08:47:58 +1100

    ensure proper display of notes in all contexts

--- client/elements/styles/sc-typography-bilara-styles.js
+++ client/elements/styles/sc-typography-bilara-styles.js
@@ -63,12 +63,6 @@ export const typographyBilaraStyles = css`
     list-style-type: none;
   }
 
-  .verse-line .translation,
-  .verse-line .root {
-    display: block;
-    margin-left: 2em;
-  }
-
   /* lookup */
 
   .spanFocused {
@@ -99,7 +93,7 @@ export const typographyBilaraStyles = css`
     padding: var(--sc-size-sm) var(--sc-size-md);
 
     color: var(--sc-secondary-text-color);
-    border-width: 0 0 0 8px;
+    border-width: 1px 0 0 8px;
     border-style: solid;
     border-color: transparent;
     border-radius: var(--sc-size-sm);
