e545e98e502b4461b3ab81f2c1b012d2d5d58d39
sujato <sujato@gmail.com>
2022-02-21 19:41:59 +1100

    first draft of footer

--- client/elements/static/sc-static-home.js
+++ client/elements/static/sc-static-home.js
@@ -302,6 +302,91 @@ class SCStaticHomePage extends SCStaticPage {
           </article>
         </section>
       </main>
+      <footer>
+        <style>
+          footer {
+            background-color: var(--sc-tertiary-background-color);
+            padding: 4em 4vw;
+          }
+          .footer-top {
+            width: 100%;
+            display: flex;
+            flex-wrap: wrap;
+            flex-direction: row;
+            justify-content: space-around;
+            align-items: left;
+            align-items: stretch;
+            gap: 2em;
+          }
+          .footer-top div {
+            flex: 1;
+            min-width: 16em;
+          }
+
+          footer ul {
+            padding: 0;
+          }
+
+          footer li {
+            padding: 1em 0 0 0;
+
+            list-style-type: none;
+          }
+
+          .footer-bottom {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-around;
+            text-align: center;
+          }
+
+          .footer-bottom li {
+            display: inline-block;
+            padding: 1em 2em 0 2em;
+          }
+        </style>
+        <section class="footer-top">
+          <div class="first">
+            <h2>Useful things</h2>
+            <ul>
+              <li>Introduction to SuttaCentral</li>
+              <li>Getting started with reading suttas</li>
+              <li>Discourses: conversations with the Buddha</li>
+              <li>Reader’s Guides</li>
+              <li>Indexes</li>
+            </ul>
+          </div>
+          <div class="second">
+            <h2>Technical information</h2>
+            <ul>
+              <li>Statistics for languages on SuttaCentral</li>
+              <li>Sutta numbering systems</li>
+              <li>Methodology for parallels</li>
+              <li>Source code on Github</li>
+            </ul>
+          </div>
+          <div class="third">
+            <h2>Related projects</h2>
+            <ul>
+              <li>Discuss and Discover—SuttaCentral forum</li>
+              <li>SuttaCentral Voice—listen to suttas</li>
+              <li>BuddhaNexus—mapping suttas with AI</li>
+              <li>
+                Reading Faithfully—Devotional and contemplative sutta reading for the faithful
+                disciple
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section class="footer-bottom">
+          <ul>
+            <li>Home</li>
+            <li>Acknowledgements</li>
+            <li>Licensing</li>
+            <li>About</li>
+          </ul>
+        </section>
+      </footer>
     `;
   }
 }
