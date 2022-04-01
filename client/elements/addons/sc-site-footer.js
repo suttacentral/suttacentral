75a8b32be81821a77da6dab25f7785c512c98ed5
ihongda <hd2935@qq.com>
2022-04-01 19:31:26 +0800

    Add footer content link.

--- client/elements/addons/sc-site-footer.js
+++ client/elements/addons/sc-site-footer.js
@@ -58,28 +58,40 @@ class ScSiteFooter extends LitElement {
           <div class="first">
             <h2>Useful things</h2>
             <ul>
-              <li>Introduction to SuttaCentral</li>
-              <li>Getting started with reading suttas</li>
-              <li>Discourses: conversations with the Buddha</li>
-              <li>Reader’s Guides</li>
-              <li>Indexes</li>
+              <li><a href="/introduction">Introduction to SuttaCentral</a></li>
+              <li><a href="/start">Getting started with reading suttas</a></li>
+              <li>
+                <a href="/discourses-guide-sujato">Discourses: conversations with the Buddha</a>
+              </li>
+              <li><a href="/general-guide-sujato">Reader’s Guides</a></li>
+              <li><a href="/subjects">Indexes</a></li>
             </ul>
           </div>
           <div class="second">
             <h2>Technical information</h2>
             <ul>
-              <li>Statistics for languages on SuttaCentral</li>
-              <li>Sutta numbering systems</li>
-              <li>Methodology for parallels</li>
-              <li>Source code on Github</li>
+              <li><a href="/languages">Statistics for languages on SuttaCentral</a></li>
+              <li><a href="/numbering">Sutta numbering systems</a></li>
+              <li><a href="/methodology">Methodology for parallels</a></li>
+              <li>
+                <a href="/https://github.com/suttacentral/suttacentral">Source code on Github</a>
+              </li>
             </ul>
           </div>
           <div class="third">
             <h2>Related projects</h2>
             <ul>
-              <li>Discuss and Discover—SuttaCentral forum</li>
-              <li>SuttaCentral Voice—listen to suttas</li>
-              <li>BuddhaNexus—mapping suttas with AI</li>
+              <li>
+                <a href="https://discourse.suttacentral.net"
+                  >Discuss and Discover—SuttaCentral forum</a
+                >
+              </li>
+              <li>
+                <a href="https://voice.suttacentral.net/scv/index.html"
+                  >SuttaCentral Voice—listen to suttas</a
+                >
+              </li>
+              <li><a href="https://buddhanexus.net">BuddhaNexus—mapping suttas with AI</a></li>
               <li>
                 Reading Faithfully—Devotional and contemplative sutta reading for the faithful
                 disciple
