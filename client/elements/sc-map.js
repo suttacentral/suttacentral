40a98057c3a1becf1521ef60cf8c053a67637b2f
Marein Könings <mail@marein.net>
2022-07-14 19:38:54 +0200

    Map tooltips, popups, layers

--- client/elements/sc-map.js
+++ client/elements/sc-map.js
@@ -4,6 +4,24 @@ import 'leaflet';
 import { LitLocalized } from './addons/sc-localization-mixin';
 import { icon } from '../img/sc-icon';
 
+class SCMapPopup extends LitLocalized(LitElement) {
+  static get properties() {
+    return {
+      name: String,
+      description: String,
+    };
+  }
+
+  render() {
+    return html`
+      <h3>${this.name}</h3>
+      ${this.description}
+    `;
+  }
+}
+
+customElements.define('sc-map-popup', SCMapPopup);
+
 export class SCMap extends LitLocalized(LitElement) {
   static properties = {
     zoom: { type: Number },
@@ -50,20 +68,17 @@ export class SCMap extends LitLocalized(LitElement) {
     // Alternative layers:
     // https://services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}
 
+    let layersControl = L.control.layers();
+
     this.fetchData().then(data =>
-      L.geoJSON(data, {
-        style: feature => feature.properties.style,
-        pointToLayer: (feature, latlng) =>
-          L.marker(latlng, {
-            icon: L.divIcon({
-              html: icon.marker[feature.properties.icon].strings, // TODO: I want to use .getHTML() here as documented, but it doesn't seem to exist https://lit.dev/docs/v1/api/lit-html/templates/#SVGTemplateResult
-              className: '',
-              iconSize: [36, 36],
-              iconAnchor: [18, 18],
-            }),
-          }),
-      }).addTo(map)
+      this.getLayerNames(data).map(layerName => {
+        let layer = this.getLayer(data, layerName);
+        map.addLayer(layer);
+        layersControl.addOverlay(layer, layerName);
+      })
     );
+
+    layersControl.addTo(map);
   }
 
   async fetchData() {
@@ -77,6 +92,35 @@ export class SCMap extends LitLocalized(LitElement) {
       .catch(error => {
         console.error('Map data fetch error:', error);
       });
+  }
+
+  getLayerNames(data) {
+    return Array.from(new Set(data.features.map(feature => feature.properties.layer)));
+  }
+
+  getLayer(data, layerName) {
+    return L.geoJSON(data, {
+      filter: (feature, layer) => feature.properties.layer == layerName,
+      style: feature => feature.properties.style,
+      onEachFeature: (feature, layer) =>
+        layer
+          .bindTooltip(feature.properties.name)
+          .bindPopup(`<h3>${feature.properties.name}</h3>${feature.properties.description}`),
+      // TODO: I want to use lit element for the popup, but this breaks with complex content (like images)
+      // .bindPopup(
+      //   `<sc-map-popup name="${feature.properties.name}" description="${feature.properties.description}"></sc-map-popup>`
+      // ),
+      pointToLayer: (feature, latlng) =>
+        L.marker(latlng, {
+          alt: feature.properties.name,
+          icon: L.divIcon({
+            html: icon.marker[feature.properties.icon].strings, // TODO: I want to use .getHTML() here as documented, but it doesn't seem to exist https://lit.dev/docs/v1/api/lit-html/templates/#SVGTemplateResult
+            className: '',
+            iconSize: [36, 36],
+            iconAnchor: [18, 18],
+          }),
+        }),
+    });
   }
 
   render() {
