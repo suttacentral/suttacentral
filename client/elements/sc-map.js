92245de44e4545026c863a0dd3e8390b117973c7
Marein Könings <mail@marein.net>
2022-10-21 20:01:31 +0200

    Implement view option for sc-map

--- client/elements/sc-map.js
+++ client/elements/sc-map.js
@@ -7,21 +7,20 @@ import { icon } from '../img/sc-icon';
 
 export class SCMap extends LitLocalized(LitElement) {
   static properties = {
-    zoom: { type: Number },
+    view: { type: String },
     longitude: { type: Number },
     latitude: { type: Number },
+    zoom: { type: Number },
   };
 
   constructor() {
     super();
 
     this.zoom = 13;
-    this.longitude = 0;
-    this.latitude = 0;
-
     this.markerScale = 3;
 
     this.mapElementID = 'map';
+    this.idToLayer = {};
   }
 
   static get styles() {
@@ -34,6 +33,12 @@ export class SCMap extends LitLocalized(LitElement) {
       #map {
         height: 480px;
         z-index: 0;
+      }
+
+      .marker-active svg path {
+        stroke: #b30309;
+        stroke-width: 6px;
+        paint-order: stroke;
       }
 
       .moving-leaflet-popup {
@@ -50,8 +55,6 @@ export class SCMap extends LitLocalized(LitElement) {
   firstUpdated() {
     this.map = L.map(this.shadowRoot.getElementById(this.mapElementID));
 
-    this.map.setView([this.latitude, this.longitude], this.zoom);
-
     this.map.addLayer(
       L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution:
@@ -62,34 +65,31 @@ export class SCMap extends LitLocalized(LitElement) {
     // Alternative layers:
     // https://services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}
 
-    let layersControl = L.control.layers();
-
-    this._fetchData().then(data =>
-      this._getLayerNames(data).map(layerName => {
-        let layer = this._getLayer(data, layerName);
-        this.map.addLayer(layer);
-        layersControl.addOverlay(layer, layerName);
-        this.map.on('zoomend', () => {
-          layer.eachLayer(l => {
-            if (l._icon) {
-              let icon = l.options.icon;
-              icon.options = Object.assign(icon.options, this._getIconZoom(this.map.getZoom()));
-              l.setIcon(icon);
-            }
-          });
-        });
-      })
-    );
-
-    this.map.addControl(layersControl);
     this.map.addControl(new L.Control.Fullscreen());
+
+    this._fetchData().then(data => {
+      this.map.addControl(
+        L.control.layers(
+          [],
+          Object.fromEntries(
+            this._getLayerNames(data).map(layerName => [
+              layerName,
+              this._getLayer(data, layerName).addTo(this.map),
+            ])
+          )
+        )
+      );
+
+      this._makeIconsZoom();
+      this._setView();
+    });
   }
 
   async _fetchData() {
     return fetch('../files/map-data.json')
       .then(response => {
         if (!response.ok) {
-          throw new Error(`'Map data fetch response is not ok: status ${response.status}`);
+          throw new Error(`Map data fetch response is not ok: status ${response.status}`);
         }
         return response.json();
       })
@@ -104,9 +104,14 @@ export class SCMap extends LitLocalized(LitElement) {
 
   _getLayer(data, layerName) {
     return L.geoJSON(data, {
-      filter: (feature, layer) => feature.properties.layer == layerName,
-      style: feature => feature.properties.style,
+      filter: feature => feature.properties.layer == layerName,
+      style: feature =>
+        Object.assign(
+          feature.properties.style,
+          this.view == feature.properties.id ? { color: '#b30309', weight: '3' } : {}
+        ),
       onEachFeature: (feature, layer) => {
+        this.idToLayer[feature.properties.id] = layer;
         layer
           .on('mouseover mousemove', event => {
             this.map.openPopup(
@@ -121,10 +126,10 @@ export class SCMap extends LitLocalized(LitElement) {
                 .setContent(feature.properties.name)
             );
           })
-          .on('mouseout', event => {
+          .on('mouseout', () => {
             this.map.closePopup();
           })
-          .on('click', event => {
+          .on('click', () => {
             if (feature.properties.define)
               window.location.href = `/define/${feature.properties.define}`;
           });
@@ -136,7 +141,7 @@ export class SCMap extends LitLocalized(LitElement) {
             Object.assign(
               {
                 html: icon.marker[feature.properties.icon].strings, // TODO: I want to use .getHTML() here as documented, but it doesn't seem to exist https://lit.dev/docs/v1/api/lit-html/templates/#SVGTemplateResult
-                className: '',
+                className: this.view == feature.properties.id ? 'marker-active' : '',
               },
               this._getIconZoom(this.zoom)
             )
@@ -145,12 +150,47 @@ export class SCMap extends LitLocalized(LitElement) {
     });
   }
 
+  _makeIconsZoom() {
+    this.map.on('zoomend', () =>
+      Object.values(this.idToLayer).map(layer => {
+        if (layer._icon) {
+          let icon = layer.options.icon;
+          icon.options = Object.assign(icon.options, this._getIconZoom(this.map.getZoom()));
+          layer.setIcon(icon);
+        }
+      })
+    );
+  }
+
   _getIconZoom(zoom) {
     let newIconSize = zoom * this.markerScale;
     return {
       iconSize: [newIconSize, newIconSize],
       iconAnchor: [newIconSize / 2, newIconSize / 2],
     };
+  }
+
+  _setView() {
+    if (this.view) {
+      if (this.idToLayer[this.view]) {
+        let layer = this.idToLayer[this.view];
+        if (layer.getBounds) {
+          this.map.fitBounds(layer.getBounds());
+        } else if (layer.getLatLng) {
+          this.map.setView(layer.getLatLng(), this.zoom);
+        }
+      } else {
+        console.error(`sc-map view option passed but no layer found with this id: ${this.view}`);
+      }
+    } else if (this.longitude !== undefined || this.latitude !== undefined) {
+      if (this.longitude !== undefined && this.latitude !== undefined) {
+        this.map.setView([this.latitude, this.longitude], this.zoom);
+      } else {
+        console.error(`sc-map: longitude or latitude is missing.`);
+      }
+    } else {
+      this.map.fitBounds(L.featureGroup(layers).getBounds());
+    }
   }
 
   render() {
