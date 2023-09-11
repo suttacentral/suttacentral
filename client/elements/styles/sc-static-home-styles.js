276baf301b18253d67e312f290ac70cdde187289
sujato <sujato@gmail.com>
2023-09-11 10:26:02 +1000

    refine m3 styles, adjust instantsearch

--- client/elements/styles/sc-static-home-styles.js
+++ client/elements/styles/sc-static-home-styles.js
@@ -1,24 +1,28 @@
 import { css } from 'lit';
 
 export const staticHomeStyles = css`
-  sc-static-home {
+  sc-static-home
+{
     font-family: var(--sc-sans-font);
     font-size: var(--sc-font-size-md);
     font-weight: 400;
 
     content-visibility: auto;
-  }
+}
 
-  main {
+main
+{
     max-width: 1600px;
     margin: 0 auto;
-  }
+}
 
-  section + section {
+section + section
+{
     margin-top: 4%;
-  }
+}
 
-  .plain {
+.plain
+{
     font-size: var(--sc-font-size-l);
 
     display: flex;
@@ -30,18 +34,20 @@ export const staticHomeStyles = css`
 
     align-items: center;
     justify-content: center;
-  }
+}
 
-  .quotation {
+.quotation
+{
     width: calc(100vw - var(--scrollbar-width));
     padding: 1em 0 1.5em 0;
 
     transform: translateX(calc((min(1600px, 100%) - 100%) / 2));
 
     background-color: var(--sc-tertiary-background-color);
-  }
+}
 
-  .quotation h2 {
+.quotation h2
+{
     font-family: var(--sc-sans-font);
     font-weight: 300;
     font-stretch: expanded;
@@ -53,9 +59,10 @@ export const staticHomeStyles = css`
     color: var(--sc-on-primary-secondary-text-color);
 
     font-variant-caps: all-small-caps;
-  }
+}
 
-  .editions {
+.editions
+{
     width: calc(100vw - var(--scrollbar-width));
     margin-top: 2rem;
     padding: 1em 0;
@@ -63,19 +70,19 @@ export const staticHomeStyles = css`
     transform: translateX(calc((min(1600px, 100%) - 100%) / 2));
 
     background-color: var(--sc-tertiary-background-color);
-  }
+}
 
-  .editions a {
-
+.editions a
+{
+    margin: 0 1em;
     padding: 1em 1em;
 
-    margin: 0 1em;
-
     border-radius: var(--sc-big-border-radius);
-  }
+}
 
 
-  .editions h2 {
+.editions h2
+{
     font-family: var(--sc-sans-font);
     font-size: var(--sc-font-size-xxxl);
     font-weight: 600;
@@ -86,12 +93,13 @@ export const staticHomeStyles = css`
 
     letter-spacing: var(--sc-caps-letter-spacing);
 
-    color: var(--sc-on-primary-secondary-text-color);
+    color: var(--sc-on-tertiary-secondary-text-color);
 
     font-variant-caps: small-caps;
-  }
+}
 
-  .editions .call-to-action {
+.editions .call-to-action
+{
     font-size: var(--sc-font-size-xl);
     font-style: italic;
 
@@ -99,14 +107,16 @@ export const staticHomeStyles = css`
 
     text-align: center;
 
-    color: var(--sc-on-primary-primary-text-color);
-  }
+    color: var(--sc-on-tertiary-secondary-text-color);
+}
 
-  .editions figure {
+.editions figure
+{
     height: 240px;
-  }
+}
 
-  .tipitaka-section {
+.tipitaka-section
+{
     font-size: var(--sc-font-size-l);
 
     display: flex;
@@ -122,9 +132,10 @@ export const staticHomeStyles = css`
     border-bottom: 2px solid var(--sc-primary-color-light);
 
     align-items: center;
-  }
+}
 
-  .tipitaka-section h2 {
+.tipitaka-section h2
+{
     font-size: var(--sc-font-size-xxl);
     line-height: 1;
 
@@ -137,24 +148,26 @@ export const staticHomeStyles = css`
     border-bottom: 1px solid var(--sc-primary-color-light);
 
     font-variant-caps: all-small-caps;
-  }
+}
 
-  .tipitaka-section h2 em {
-    font-size: 0.6667em;
+.tipitaka-section h2 em
+{
+    font-size: .6667em;
     font-weight: 500;
     font-style: italic;
 
     display: inline-block;
 
-    height: 0.9em;
+    height: .9em;
 
     vertical-align: middle;
     letter-spacing: normal;
 
     font-variant-caps: normal;
-  }
+}
 
-  blockquote {
+blockquote
+{
     font-family: var(--sc-serif-font);
     font-size: var(--sc-font-size-xl);
     font-weight: 300;
@@ -168,26 +181,29 @@ export const staticHomeStyles = css`
     padding: 0;
 
     text-align: center;
-  }
+}
 
-  /* ensure a consistent spacing for the quote decoration */
-  blockquote span {
+/* ensure a consistent spacing for the quote decoration */
+blockquote span
+{
     position: relative;
-  }
+}
 
-  blockquote span:before {
+blockquote span:before
+{
     font-size: 4em;
     line-height: 1;
 
     position: absolute;
-    left: -0.5em;
+    left: -.5em;
 
     content: '“';
 
     color: var(--sc-icon-color);
-  }
+}
 
-  article {
+article
+{
     display: flex;
     overflow: hidden;
     flex-direction: column;
@@ -195,9 +211,10 @@ export const staticHomeStyles = css`
     margin-bottom: 16px;
 
     border-radius: var(--sc-big-border-radius);
-  }
+}
 
-  .two-cards {
+.two-cards
+{
     display: grid;
 
     margin-right: 1vw;
@@ -206,24 +223,27 @@ export const staticHomeStyles = css`
     justify-content: center;
     grid-template-columns: repeat(2, minmax(240px, 1fr));
     gap: 3% 2%;
-  }
+}
 
-  figure {
+figure
+{
     position: relative;
 
     overflow: hidden;
 
     height: 480px;
     margin: 0;
-  }
+}
 
-  img {
+img
+{
     height: 100%;
 
     object-fit: cover;
-  }
+}
 
-  figcaption {
+figcaption
+{
     font-size: var(--sc-font-size-s);
     font-weight: 600;
 
@@ -236,28 +256,36 @@ export const staticHomeStyles = css`
     padding: 16px;
 
     text-align: right;
-    letter-spacing: 0.5px;
+    letter-spacing: .5px;
 
     color: white;
-    background-color: rgba(0, 0, 0, 0.5);
+    background-color: rgba(0, 0, 0, .5);
 
     backdrop-filter: blur(2px);
-  }
+}
 
-  .card .block-link {
+  .card 
+{
+   position: relative;
 
+   --md-elevation-level: 3;
+}
+
+.card .block-link
+{
     height: -moz-available;          /* WebKit-based browsers will ignore this. */
     height: -webkit-fill-available;  /* Mozilla-based browsers will ignore this. */
     height: stretch;
-  }
+}
 
-  .two-cards .card{
+.two-cards .card
+{
+    color: var(--sc-on-secondary-primary-text-color);
+    background-color: var(--sc-tertiary-background-color);
+}
 
-        background-color: var(--sc-tertiary-background-color);
-        color: var(--sc-on-secondary-primary-text-color);
-  }
-
-  .card-content {
+.card-content
+{
     display: flex;
     flex-direction: column;
 
@@ -265,75 +293,86 @@ export const staticHomeStyles = css`
 
     justify-content: space-between;
     flex: 1;
+}
 
-  }
-
-  .card-text {
+.card-text
+{
     padding: 1.5em;
-  }
+}
 
-  .card-actions {
+.card-actions
+{
     display: flex;
 
     margin: 8px 0;
 
     align-items: flex-end;
     justify-content: flex-end;
-  }
+}
 
-  h2 {
+h2
+{
     font-family: var(--sc-serif-font);
     font-size: var(--sc-font-size-xl);
     font-weight: 500;
 
     margin: 0 0 0 0;
-  }
+}
 
-  p {
+p
+{
     font-size: var(--sc-font-size-md);
     line-height: 1.5;
 
-    margin: 0.5em 0 0 0;
-  }
+    margin: .5em 0 0 0;
+}
 
-  .video {
+.video
+{
     display: flex;
 
     height: 360px;
 
     justify-content: center;
-  }
+}
 
-  video {
+video
+{
     border-radius: var(--sc-mid-border-radius);
     box-shadow: var(--sc-shadow-elevation-2dp);
-  }
+}
 
-  @media (max-width: 680px) {
-    .two-cards {
-      display: flex;
-      flex-direction: column;
+@media (max-width: 680px)
+{
+    .two-cards
+    {
+        display: flex;
+        flex-direction: column;
 
-      margin-right: 0;
-      margin-left: 0;
+        margin-right: 0;
+        margin-left: 0;
 
-      gap: 0;
+        gap: 0;
     }
 
-    video {
-      width: 100%;
+    video
+    {
+        width: 100%;
     }
 
-    br {
-      content: '';
+    br
+    {
+        content: '';
     }
 
-    h2 {
-      font-weight: 500;
+    h2
+    {
+        font-weight: 500;
     }
-  }
+}
 
-  .top-two {
+.top-two
+{
     display: flex;
     flex-direction: row;
 
@@ -343,34 +382,40 @@ export const staticHomeStyles = css`
     flex-wrap: nowrap;
     justify-content: space-around;
     gap: 1em;
-  }
+}
 
-  @media (max-width: 1120px) {
-    .top-two {
-      flex-wrap: wrap;
+@media (max-width: 1120px)
+{
+    .top-two
+    {
+        flex-wrap: wrap;
     }
-  }
+}
 
-  .top-two .plain {
+.top-two .plain
+{
     margin-top: 0;
-  }
+}
 
-  .sc-related {
+.sc-related
+{
     display: flex;
     flex-direction: column;
 
     margin: 4% 1vw;
 
     gap: 1vw;
-  }
+}
 
-  .sc-related article {
+.sc-related article
+{
     color: var(--sc-on-primary-primary-text-color);
 
     flex: 1;
-  }
+}
 
-  .sc-related article header {
+.sc-related article header
+{
     display: flex;
 
     height: 80px;
@@ -379,13 +424,15 @@ export const staticHomeStyles = css`
     color: white;
 
     justify-content: space-between;
-  }
+}
 
-  .sc-related article img {
+.sc-related article img
+{
     width: 72px;
-  }
+}
 
-  .sc-related article header h3 {
+.sc-related article header h3
+{
     font-family: var(--sc-sans-font);
     font-size: var(--sc-font-size-l);
     font-weight: 600;
@@ -396,85 +443,98 @@ export const staticHomeStyles = css`
     margin: 0;
 
     text-align: end;
-  }
+}
 
-  .sc-related-item-subtitle {
+.sc-related-item-subtitle
+{
     font-weight: 400;
     font-style: italic;
-  }
+}
 
-  .related-projects-content {
+.related-projects-content
+{
+    position: relative;
+
     margin: 1em 1em 1.5em;
 
     color: var(--sc-on-secondary-primary-text-color);
-    position: relative;
-  }
+}
 
-  .sc-related article p {
+.sc-related article p
+{
     margin-top: 0;
-  }
+}
 
-  .sc-related article ul {
-    margin: 0.75em 0 0 0;
+.sc-related article ul
+{
+    margin: .75em 0 0 0;
     padding: 0 0 0 2em;
-  }
+}
 
-  li::marker {
+li::marker
+{
     font-family: var(--sc-sans-font);
     font-weight: 600;
 
     color: var(--sc-icon-color);
 
     font-feature-settings: 'tnum', 'onum';
-  }
+}
 
-  .sc-related-items-wrapper {
+.sc-related-items-wrapper
+{
     display: flex;
     flex-direction: row;
 
     justify-content: center;
     flex-wrap: wrap;
     gap: 1em;
-  }
+}
 
-  .card {
-    position: relative;
-    --md-elevation-level: 3;
-  }
 
-  .secondary-accent header {
+
+.secondary-accent header
+{
     background-color: var(--sc-secondary-accent-color);
-  }
+}
 
-  .secondary-accent {
+.secondary-accent
+{
     outline: 2px solid var(--sc-secondary-accent-color);
-  }
+}
 
-  .dark-accent header {
+.dark-accent header
+{
     background-color: var(--sc-on-primary-secondary-text-color);
-  }
+}
 
-  .dark-accent {
+.dark-accent
+{
     outline: 2px solid var(--sc-on-primary-secondary-text-color);
-  }
+}
 
-  .primary-accent header {
+.primary-accent header
+{
     background-color: var(--sc-primary-accent-color);
-  }
+}
 
-  .primary-accent {
+.primary-accent
+{
     outline: 2px solid var(--sc-primary-accent-color);
-  }
+}
 
-  .primary-color header {
+.primary-color header
+{
     background-color: var(--sc-primary-color);
-  }
+}
 
-  .primary-color {
+.primary-color
+{
     outline: 2px solid var(--sc-primary-color);
-  }
+}
 
-  .related-projects-heading {
+.related-projects-heading
+{
     display: flex;
     flex-direction: row;
 
@@ -484,9 +544,10 @@ export const staticHomeStyles = css`
     border-bottom: 2px solid var(--sc-border-color);
 
     justify-content: space-between;
-  }
+}
 
-  .related-projects-heading h2 {
+.related-projects-heading h2
+{
     font-family: var(--sc-sans-font);
     font-weight: 300;
     font-stretch: expanded;
@@ -496,9 +557,10 @@ export const staticHomeStyles = css`
     color: var(--sc-on-primary-secondary-text-color);
 
     font-variant-caps: all-small-caps;
-  }
+}
 
-  .related-projects-heading a {
+.related-projects-heading a
+{
     display: flex;
     flex-direction: row;
 
@@ -509,52 +571,63 @@ export const staticHomeStyles = css`
 
     gap: 1em;
     align-items: center;
-  }
+}
 
-  .sc-related-items-wrapper a {
+.sc-related-items-wrapper a
+{
     height: 100%;
 
     color: var(--sc-on-secondary-primary-text-color);
     background-color: var(--sc-secondary-background-color);
-  }
+}
 
-  .related-projects-heading a:active {
+.related-projects-heading a:active
+{
     background-color: var(--sc-primary-color-light);
-  }
+}
 
-  .related-projects-heading .icon {
+.related-projects-heading .icon
+{
     fill: var(--sc-icon-color);
-  }
+}
 
-  @media (max-width: 1120px) {
-    .sc-related article {
-      min-width: 40%;
+@media (max-width: 1120px)
+{
+    .sc-related article
+    {
+        min-width: 40%;
     }
-  }
+}
 
-  @media (max-width: 560px) {
-    .sc-related article {
-      min-width: 80%;
+@media (max-width: 560px)
+{
+    .sc-related article
+    {
+        min-width: 80%;
     }
-  }
+}
 
-  .people,
-  .speaker {
+.people,
+.speaker
+{
     margin: 1rem;
 
     transform: scale(2);
 
     fill: white;
-  }
+}
 
-  .bilara {
+.bilara
+{
     width: 60px;
     height: 60px;
 
     fill: white;
-  }
+}
 
-  sc-navigation-tipitaka {
+sc-navigation-tipitaka
+{
     min-height: 270px;
-  }
+}
+
 `;
