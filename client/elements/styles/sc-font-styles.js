4d348d0405c9bc192b8ee9927ba8525ffc344d76
sujato <sujato@gmail.com>
2023-09-22 18:20:33 +1000

    add major second type scale for mobile, adjust heading leading

--- client/elements/styles/sc-font-styles.js
+++ client/elements/styles/sc-font-styles.js
@@ -1406,4 +1406,28 @@ export const SCFontStyles = css`
 
     --sc-caps-letter-spacing: 0.02em;
   }
+
+      /* for small screen, use type scale at 1.125 for larger font sizes, keep smaller sizes the same */
+
+      @media screen and (max-width: 600px) {
+  html {
+    --sc-font-size-xxs: 0.65rem;
+
+    --sc-font-size-xs: 0.78rem;
+
+    --sc-font-size-s: 0.94rem;
+
+    --sc-font-size-md: 1.125rem;
+
+    --sc-font-size-l: 1.265rem;
+
+    --sc-font-size-xl: 1.424rem;
+
+    --sc-font-size-xxl: 1.6rem;
+
+    --sc-font-size-xxxl: 1.8rem;
+
+    --sc-font-size-xxxxl: 2.03rem;
+  }
+}
 `;
