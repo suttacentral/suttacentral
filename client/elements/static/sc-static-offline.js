8a6ec4a6d7fed77f8096f472f70987f836306220
ihongda <hd2935@qq.com>
2023-09-26 15:23:27 +0800

    fix typo.

--- client/elements/static/sc-static-offline.js
+++ client/elements/static/sc-static-offline.js
@@ -496,10 +496,8 @@ export class SCStaticOffline extends LitLocalized(LitElement) {
       md-outlined-button {
         --md-sys-color-primary: var(--sc-primary-accent-color);
         --md-sys-color-on-primary: white;
-        --md-filled-button-label-text-font: var(--sc-sans-font);
-        --md-filled-button-label-text-size: var(--sc-size-md);
         --md-outlined-button-label-text-font: var(--sc-sans-font);
-        --mmd-outlined-button-label-text-size: var(--sc-size-md);
+        --md-outlined-button-label-text-size: var(--sc-size-md);
       }
 
       .option-multi-select {
