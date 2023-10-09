93a69fd5fc01f4e4f28dfe11192734d4530b91ad
ihongda <hd2935@qq.com>
2023-10-09 13:02:14 +0800

    Adjust instant search data storage logic.

--- server/server/search/immediate_search.py
+++ server/server/search/immediate_search.py
@@ -1,31 +1,6 @@
 from common.arangodb import get_db
 
 
-AQL_POSSIBLE_RESULTS = '''
-LET possible_uids = (
-    FOR doc IN super_nav_details
-        FILTER doc.uid like @uid
-        LIMIT 10
-        RETURN {
-            uid:doc.uid,
-            title:doc.name
-        }
-)
-
-LET possible_names = (
-    FOR d IN instant_search
-        SEARCH PHRASE(d.name, @name, "common_text")
-        FILTER d.is_segmented != True AND (d.lang == @lang OR d.lang in (FOR l IN language FILTER l.is_root == true RETURN l.uid))
-        LIMIT 10
-    RETURN {
-        uid:d.uid,
-        title:d.name
-    }
-)
-
-RETURN UNION(possible_uids, possible_names)
-'''
-
 AQL_POSSIBLE_RESULTS_BY_LANG = '''
     LET ebs_prefixes = ["dn", "da", "mn", "ma", "sn", "sa", "sa-2", "sa-3", "an", "ea", "ea-2", "kp", "iti", "ud", "snp",
                 "dhp", "thig", "thag", "sf"]
@@ -42,10 +17,10 @@ AQL_POSSIBLE_RESULTS_BY_LANG = '''
             FILTER CONTAINS(d.uid, item)
             RETURN item
         )[0]
-        FILTER root_uid in ebs_prefixes
+        FILTER root_uid IN ebs_prefixes OR d.uid IN ebs_prefixes
     RETURN {
-        uid:d.uid,
-        title:d.name,
+        uid: d.uid,
+        title: d.name,
         isRoot: d.is_root,
         nodeType: navigation_doc.type
     }
@@ -53,6 +28,29 @@ AQL_POSSIBLE_RESULTS_BY_LANG = '''
 
 
 def fetch_possible_result(lang):
+    """
+    Fetches possible results based on the specified language.
+
+    Args:
+        lang (str): The language to fetch results for.
+
+    Returns:
+        list: A list of merged results.
+        int: The HTTP status code.
+    """
     db = get_db()
     data = list(db.aql.execute(AQL_POSSIBLE_RESULTS_BY_LANG, bind_vars={'lang': lang}))
-    return data, 200
+
+    merged_data = {}
+    for item in data:
+        uid = item["uid"]
+        title = item["title"]
+
+        if uid in merged_data:
+            merged_data[uid]["title"] += f"-{title}"
+        else:
+            merged_data[uid] = {"uid": uid, "title": title, "nodeType": item['nodeType']}
+
+    merged_list = list(merged_data.values())
+
+    return merged_list, 200
