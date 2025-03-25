cdeb15e34cb1faa4b97e328c5d38448c57f75917
jr <jhanarato@gmail.com>
2025-03-25 13:57:35 +0800

    Add test for data load testing database name.

--- server/server/data_loader/tests/test_arangoload.py
+++ server/server/data_loader/tests/test_arangoload.py
@@ -3,3 +3,7 @@ from config import TestingConfig
 def test_normal_testing_database_name():
     config = TestingConfig()
     assert config.ARANGO_DB == "suttacentral_tests"
+
+def test_data_load_testing_database_name():
+    config = TestingConfig()
+    assert config.ARANGO_DB_DATA_LOAD_TEST == "suttacentral_data_load_tests"
