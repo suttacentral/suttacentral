1224e49605277d5c76bb2b4a5b752509d56fcd3d
jr <jhanarato@gmail.com>
2025-03-26 15:19:57 +0800

    Introduce data_load_app fixture

--- server/server/data_loader/tests/test_arangoload.py
+++ server/server/data_loader/tests/test_arangoload.py
@@ -1,9 +1,14 @@
+import pytest
+
 from common.arangodb import get_db
 from common.utils import current_app
 
-
-def test_set_db_name():
+@pytest.fixture
+def data_load_app(app):
     app = current_app()
     app.config['ARANGO_DB'] = 'suttacentral_data_load_tests'
-    with app.app_context():
+    return app
+
+def test_set_db_name(data_load_app):
+    with data_load_app.app_context():
         assert get_db().name == 'suttacentral_data_load_tests'
