adc0ac6f8e9e28cde9c5780630bb8a0cf072b2bf
jr <jhanarato@gmail.com>
2025-03-26 15:59:31 +0800

    Account for BASE_DIR being different due to current working directory being different.

--- server/server/data_loader/tests/test_arangoload.py
+++ server/server/data_loader/tests/test_arangoload.py
@@ -11,11 +11,17 @@ from data_loader import arangoload
 def data_load_app(app):
     app = current_app()
     app.config['ARANGO_DB'] = 'suttacentral_data_load_tests'
+    app.config['BASE_DIR'] = '/opt/sc/sc-flask/'
     return app
 
 def test_set_db_name(data_load_app):
     with data_load_app.app_context():
         assert get_db().name == 'suttacentral_data_load_tests'
+
+def test_base_dir_is_correct(data_load_app):
+    with data_load_app.app_context():
+        base_dir = data_load_app.config.get('BASE_DIR')
+        assert base_dir == '/opt/sc/sc-flask/'
 
 def test_do_collect_data_stage(data_load_app):
     with data_load_app.app_context():
@@ -23,7 +29,7 @@ def test_do_collect_data_stage(data_load_app):
         git_repository = data_load_app.config.get('DATA_REPO')
         arangoload.collect_data(data_dir, git_repository)
 
-@pytest.mark.skip("Figure out why collect_data() fails first")
+@pytest.mark.skip('Figure out why collect_data() fails first')
 def test_do_entire_run(data_load_app):
     with data_load_app.app_context():
         arangoload.run()
