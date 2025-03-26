39201b5e29dfaf9d5878052225a838246564bb1a
jr <jhanarato@gmail.com>
2025-03-26 15:12:08 +0800

    Hard code database name in test file.
    We now have a single test that can run get_db() and specify the full database name.

--- server/server/data_loader/tests/test_arangoload.py
+++ server/server/data_loader/tests/test_arangoload.py
@@ -1,25 +1,9 @@
-import pytest
-
-from config import TestingConfig
-from data_loader.arangoload import run
 from common.arangodb import get_db
-from common.utils import app_context, current_app
+from common.utils import current_app
 
-
-def test_normal_testing_db_name():
-    config = TestingConfig()
-    assert config.ARANGO_DB == 'suttacentral_tests'
-
-def test_data_load_tests_db_name():
-    config = TestingConfig()
-    assert config.ARANGO_DB_DATA_LOAD_TEST == 'suttacentral_data_load_tests'
-
-def test_get_application_context():
-    context = current_app().app_context()
-    assert context
 
 def test_set_db_name():
     app = current_app()
     app.config['ARANGO_DB'] = 'suttacentral_data_load_tests'
-    with app.app_context() as context:
+    with app.app_context():
         assert get_db().name == 'suttacentral_data_load_tests'
