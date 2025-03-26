e4073109ca9d747dccfa79d9bb295a07753ef179
jr <jhanarato@gmail.com>
2025-03-26 15:44:37 +0800

    collect_data() runs in pytest

--- server/server/data_loader/tests/test_arangoload.py
+++ server/server/data_loader/tests/test_arangoload.py
@@ -1,7 +1,11 @@
+from pathlib import Path
+
 import pytest
 
 from common.arangodb import get_db
 from common.utils import current_app
+from data_loader import arangoload
+
 
 @pytest.fixture
 def data_load_app(app):
@@ -12,3 +16,14 @@ def data_load_app(app):
 def test_set_db_name(data_load_app):
     with data_load_app.app_context():
         assert get_db().name == 'suttacentral_data_load_tests'
+
+def test_do_collect_data_stage(data_load_app):
+    with data_load_app.app_context():
+        data_dir = Path('/opt/sc/sc-flask/sc-data')
+        git_repository = data_load_app.config.get('DATA_REPO')
+        arangoload.collect_data(data_dir, git_repository)
+
+@pytest.mark.skip("Figure out why collect_data() fails first")
+def test_do_entire_run(data_load_app):
+    with data_load_app.app_context():
+        arangoload.run()
