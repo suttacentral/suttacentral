a086bb2f1df570f7050b62e3be46d568a8cfc3c1
jr <jhanarato@gmail.com>
2025-04-18 09:46:07 +0800

    Rewrite docstring tests with pytest.

---
+++ server/server/data_loader/tests/test_sc_html.py
@@ -0,0 +1,30 @@
+from data_loader.sc_html import fromstring
+
+
+class TestHtHtmlElementMixin:
+    def test_detach(self):
+        p = fromstring('<p><em>p1</em>Once upon a time...</p>')
+        p.append(p.find('em').detach())
+        assert str(p) == '<p>Once upon a time...<em>p1</em></p>'
+
+    def test_prepend(self):
+        p = fromstring('<p>There can be only one.</p>')
+        p.insert(0, p.makeelement('a', {'id': 'wrong'}))
+        assert str(p) == '<p>There can be only one.<a id="wrong"></a></p>'
+        p.prepend(p.makeelement('a', {'id': 'right'}))
+        assert str(p) == '<p><a id="right"></a>There can be only one.<a id="wrong"></a></p>'
+
+    def test_wrap_outer(self):
+        dom = fromstring('<div><a>foo</a>bar</div>')
+        dom.find('a').wrap_outer(dom.makeelement('b'))
+        assert str(dom) == '<div><b><a>foo</a></b>bar</div>'
+
+    def test_wrap_inner(self):
+        dom = fromstring('<div><a>foo</a>bar</div>')
+        dom.find('a').wrap_inner(dom.makeelement('i'))
+        assert str(dom) == '<div><a><i>foo</i></a>bar</div>'
+
+    def test_convert_bad_tags(self):
+        dom = fromstring('<baa><p><moo>Goes the cow</namo> ...')
+        dom.convert_bad_tags()
+        assert str(dom) == '<div class="baa"><p><span class="moo">Goes the cow ...</span></p></div>'
