47d8e800213b195a53e5991ce31cf02179b33e2c
jr <jhanarato@gmail.com>
2025-05-31 10:26:53 +0800

    Broke up parameterised test, renamed a test.

--- server/server/data_loader/tests/test_unsegmented_texts.py
+++ server/server/data_loader/tests/test_unsegmented_texts.py
@@ -7,19 +7,22 @@ from data_loader.unsegmented_texts import UnsegmentedText
 
 
 class TestUnsegmentedText:
-    @pytest.mark.parametrize(
-    "html,author_long_name",
-        [
-            ("<html><meta name='author' content='Bhikkhu Bodhi'></html>", 'Bhikkhu Bodhi'),
-            ("<html><meta author='Bhikkhu Bodhi'></html>", 'Bhikkhu Bodhi'),
-            ("<html></html>", None),
-        ]
-    )
-    def test_extracts_author_long_name_from_html(self, html, author_long_name):
-        text = UnsegmentedText(file=Path('/'), html=html, lang_uid="en")
-        assert text.authors_long_name() == author_long_name
+    def test_extracts_authors_long_name_from_content_attribute(self):
+        html = "<html><meta name='author' content='Bhikkhu Bodhi'></html>"
+        text = UnsegmentedText(file=Path('mn1.html'), html=html, lang_uid='en')
+        assert text.authors_long_name() == 'Bhikkhu Bodhi'
 
-    def test_logs_author_not_found(self, caplog):
+    def test_extracts_authors_long_name_from_author_attribute(self):
+        html = "<html><meta author='Bhikkhu Bodhi'></html>"
+        text = UnsegmentedText(file=Path('mn1.html'), html=html, lang_uid='en')
+        assert text.authors_long_name() == 'Bhikkhu Bodhi'
+
+    def test_authors_long_name_is_none_when_missing(self):
+        html = "<html></html>"
+        text = UnsegmentedText(file=Path('mn1.html'), html=html, lang_uid='en')
+        assert text.authors_long_name() is None
+
+    def test_logs_missing_authors_long_name(self, caplog):
         html = "<html></html>"
         text = UnsegmentedText(file=Path('missing.html'), html=html, lang_uid="en")
         _ = text.authors_long_name()
