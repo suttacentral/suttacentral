b9290a6df20d20fcdc0da7434e4c321a776b73ab
jr <jhanarato@gmail.com>
2025-12-29 10:43:02 +0800

    Use open-wc fixtures to test connectedCallback()

--- client/test/learn-lit-lifecycle.test.js
+++ client/test/learn-lit-lifecycle.test.js
@@ -1,5 +1,6 @@
 // These tests are for learning only and do not test any SuttaCentral code.
 
+import { html as htmlTesting, fixture } from '@open-wc/testing';
 import { assert } from '@esm-bundle/chai';
 
 import { html, LitElement } from "lit";
@@ -12,6 +13,12 @@ class TestElement extends LitElement {
   constructor() {
     super();
     this.string_property = 'Initial value';
+    this.calledConnectedCallback = false;
+  }
+
+  connectedCallback() {
+    this.calledConnectedCallback = true;
+    super.connectedCallback();
   }
 
   render() {
@@ -21,8 +28,13 @@ class TestElement extends LitElement {
 customElements.define('test-element', TestElement);
 
 describe('TestElement', () => {
-  it('Should set the string property when constructed', () => {
+  it('Should initialise string_property on construction', () => {
     let sut = new TestElement();
     assert.equal(sut.string_property, 'Initial value');
+  });
+
+  it('Should call connectedCallback() when attached to DOM', async () => {
+    const element = await fixture(htmlTesting`<test-element></test-element>`)
+    assert(element.calledConnectedCallback);
   });
 })
