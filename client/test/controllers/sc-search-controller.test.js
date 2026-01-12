5ea8c3f0fc9a4ada44e45b2bb4e3a35076f046cf
jr <jhanarato@gmail.com>
2026-01-12 19:39:59 +0800

    Add test for changed requestUrl
    Issue #3536.

--- client/test/controllers/sc-search-controller.test.js
+++ client/test/controllers/sc-search-controller.test.js
@@ -1,15 +1,23 @@
-import { SCSearchController} from "../../elements/sc-search-controller";
+import {SCSearchController} from "../../elements/sc-search-controller";
 
-import { assert } from '@esm-bundle/chai';
+import {assert} from '@esm-bundle/chai';
 
 const FIRST_RESPONSE = Response.json({ response_num: 1 });
 
 describe('SCSearchController', () => {
   it(`should fetch first time`, async () => {
-    let response = Response.json({ response_num: 1 });
     let controller = new SCSearchController();
-    controller.stubbedResponse(response);
+    controller.stubbedResponse(Response.json({response_num: 1}));
     let result = await controller.fetchResult(`http://example.com/api/abc`, `['en']`);
     assert.equal(result['response_num'], 1);
-  })
+  });
+
+  it(`should fetch if requestUrl changes`, async () => {
+    let controller = new SCSearchController();
+    controller.stubbedResponse(Response.json({response_num: 1}));
+    let result_one = await controller.fetchResult(`http://example.com/api/abc`, `['en']`);
+    controller.stubbedResponse(Response.json({response_num: 2}));
+    let result_two = await controller.fetchResult(`http://example.com/api/def`, `['en']`);
+    assert.equal(result_two['response_num'], 2);
+  });
 });
