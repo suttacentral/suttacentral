85fa1641e04ef13ba60445cb427ca8413e5c8c41
jr <jhanarato@gmail.com>
2026-01-12 17:48:39 +0800

    Add stubbed result for testing.
    Issue #3536.

--- client/elements/sc-search-controller.js
+++ client/elements/sc-search-controller.js
@@ -1,5 +1,17 @@
 export class SCSearchController {
+  constructor() {
+    this._stubbedResult = null;
+  }
+
+  stubbedResult(value) {
+    this._stubbedResult = value;
+  }
+
   async fetchResult(requestUrl, selectedLanguages) {
+    if(this._stubbedResult) {
+      return this._stubbedResult
+    }
+
     return await (
       await fetch(requestUrl, {
         method: 'POST',
--- client/test/controllers/sc-search-controller.test.js
+++ client/test/controllers/sc-search-controller.test.js
@@ -3,7 +3,13 @@ import { SCSearchController} from "../../elements/sc-search-controller";
 import { assert } from '@esm-bundle/chai';
 
 describe('SCSearchController', () => {
-  it('should be created', () => {
+  it(`should fetch stubbed results`, async () => {
     let controller = new SCSearchController();
-  });
+    controller.stubbedResult('some response');
+    let result = await controller.fetchResult(
+      `http://localhost/api/search/instant?limit=50&query=ghosts&language=en&restrict=all&matchpartial=false`,
+      `['en', 'pli']`
+    );
+    assert.equal(result, 'some response');
+  })
 });
