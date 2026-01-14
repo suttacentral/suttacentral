7a3174a02c26bc28f8deaeabfebdf8a1589646eb
jr <jhanarato@gmail.com>
2026-01-14 19:13:18 +0800

    Use makeResponses() in one test.
    Issue #3536.

--- client/test/controllers/sc-search-controller.test.js
+++ client/test/controllers/sc-search-controller.test.js
@@ -21,44 +21,29 @@ describe(`makeResponses()`, () => {
 
 describe('SCSearchController', () => {
   it(`should fetch first time`, async () => {
-    let controller = new SCSearchController([Response.json({response_num: 1})]);
+    let controller = new SCSearchController(makeResponses());
     let result = await controller.fetchResult(`http://example.com/api/abc`, ['en']);
-    assert.equal(result['response_num'], 1);
+    assert.equal(result['id'], 1);
   });
 
   it(`should fetch if requestUrl changes`, async () => {
-    let controller = new SCSearchController(
-      [
-        Response.json({response_num: 2}),
-        Response.json({response_num: 1}),
-      ]
-    );
-    let result_one = await controller.fetchResult(`http://example.com/api/abc`, ['en']);
-    let result_two = await controller.fetchResult(`http://example.com/api/def`, ['en']);
-    assert.equal(result_two['response_num'], 2);
+    let controller = new SCSearchController(makeResponses());
+    await controller.fetchResult(`http://example.com/api/abc`, ['en']);
+    let result = await controller.fetchResult(`http://example.com/api/def`, ['en']);
+    assert.equal(result['id'], 2);
   });
 
   it(`should fetch if selectedLanguages changes`, async () => {
-    let controller = new SCSearchController(
-      [
-        Response.json({response_num: 2}),
-        Response.json({response_num: 1}),
-      ]
-    );
-    let result_one = await controller.fetchResult(`http://example.com/api/abc`, ['en']);
-    let result_two = await controller.fetchResult(`http://example.com/api/abc`, ['en', 'pli']);
-    assert.equal(result_two['response_num'], 2);
+    let controller = new SCSearchController(makeResponses());
+    await controller.fetchResult(`http://example.com/api/abc`, ['en']);
+    let result = await controller.fetchResult(`http://example.com/api/abc`, ['en', 'pli']);
+    assert.equal(result['id'], 2);
   });
 
   it(`should fetch if both change`, async () => {
-    let controller = new SCSearchController(
-      [
-        Response.json({response_num: 2}),
-        Response.json({response_num: 1}),
-      ]
-    );
-    let result_one = await controller.fetchResult(`http://example.com/api/abc`, ['en']);
-    let result_two = await controller.fetchResult(`http://example.com/api/def`, ['en', 'pli']);
-    assert.equal(result_two['response_num'], 2);
+    let controller = new SCSearchController(makeResponses());
+    await controller.fetchResult(`http://example.com/api/abc`, ['en']);
+    let result = await controller.fetchResult(`http://example.com/api/def`, ['en', 'pli']);
+    assert.equal(result['id'], 2);
   });
 });
