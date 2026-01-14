ae2605b8bb0b332b7b9e26b5383d131d78494c8e
jr <jhanarato@gmail.com>
2026-01-14 13:07:27 +0800

    Move function back into class. Dang.
    Issue #3536.

--- client/elements/sc-search-controller.js
+++ client/elements/sc-search-controller.js
@@ -1,13 +1,3 @@
-async function requestSearchResults(requestUrl, selectedLanguages) {
-  return await fetch(requestUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(selectedLanguages),
-  });
-}
-
 export class SCSearchController {
   constructor() {
     this._stubbedResponse = null;
@@ -17,13 +7,23 @@ export class SCSearchController {
     this._stubbedResponse = response;
   }
 
+  async #requestSearchResults(requestUrl, selectedLanguages) {
+    return await fetch(requestUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(selectedLanguages),
+    });
+  }
+
   async fetchResult(requestUrl, selectedLanguages) {
     let response;
 
     if(this._stubbedResponse) {
       response = await this._stubbedResponse
     } else {
-      response = await requestSearchResults(requestUrl, selectedLanguages);
+      response = await this.#requestSearchResults(requestUrl, selectedLanguages);
     }
 
     return await response.json();
