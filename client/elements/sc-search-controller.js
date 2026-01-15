33cb4eb38dc646ef6baa573c3da9f986c71470c1
jr <jhanarato@gmail.com>
2026-01-15 09:22:39 +0800

    Make object a class.
    Issue #3536.

--- client/elements/sc-search-controller.js
+++ client/elements/sc-search-controller.js
@@ -1,13 +1,17 @@
+class RequestData {
+  constructor(url, selectedLanguages) {
+    this.url = url;
+    this.body = JSON.stringify(selectedLanguages);
+  }
+}
+
 export class SCSearchController {
   constructor(cannedResponses = null) {
     this._cannedResponses = cannedResponses;
   }
 
   async fetchResult(url, selectedLanguages) {
-    let requestData = {
-      url: url,
-      body: JSON.stringify(selectedLanguages),
-    }
+    let requestData = new RequestData(url, selectedLanguages);
     let request = this.#createRequest(requestData);
     let response = await this.#getResponse(request);
     return await response.json();
