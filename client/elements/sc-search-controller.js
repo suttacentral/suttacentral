bb480d34baafa83bd648abe42ab00af9f299945e
jr <jhanarato@gmail.com>
2026-01-15 09:05:01 +0800

    Various tidying for SCSearchController.
    Issue #3536.

--- client/elements/sc-search-controller.js
+++ client/elements/sc-search-controller.js
@@ -3,27 +3,23 @@ export class SCSearchController {
     this._cannedResponses = cannedResponses;
   }
 
-  async fetchResult(requestUrl, selectedLanguages) {
-    let request = this.#createRequest(requestUrl, selectedLanguages);
+  async fetchResult(url, selectedLanguages) {
+    let body = JSON.stringify(selectedLanguages);
+    let request = this.#createRequest(url, body);
     let response = await this.#getResponse(request);
     return await response.json();
   }
 
-  #createRequest(requestUrl, selectedLanguages) {
-    return new Request(requestUrl, {
+  #createRequest(url, body) {
+    return new Request(url, {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(selectedLanguages),
+      headers: { 'Content-Type': 'application/json' },
+      body: body,
     });
   }
 
   async #getResponse(request) {
-    if(this._cannedResponses) {
-      return await this._cannedResponses.pop();
-    } else {
-      return await fetch(request);
-    }
+    if(this._cannedResponses) return await this._cannedResponses.pop();
+    return await fetch(request);
   }
 }
