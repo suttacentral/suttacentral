e2fddbc8e1d1b20607ecad132338b6826db43df6
jr <jhanarato@gmail.com>
2026-02-23 10:17:49 +0800

    Remove path setting in wsgi.py
    We're importing app from the installed sc_flask package so this hack isn't needed.
    
    Issue #3564

--- server/server/wsgi.py
+++ server/server/wsgi.py
@@ -1,10 +1,4 @@
-import os
-import sys
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(dir_path)
-
-from sc_flask.app import app  # isort:skip
+from sc_flask.app import app
 
 
 if __name__ == '__main__':
