22969f779c65e30f89e4754cdccd365fde1ceecf
Ajahn J.R. <872786+jhanarato@users.noreply.github.com>
2026-03-26 12:10:59 +0800

    Double quote variable names
    Co-authored-by: Anton Gilgur <4970083+agilgur5@users.noreply.github.com>

--- arangodb/healthcheck.sh
+++ arangodb/healthcheck.sh
@@ -3,9 +3,9 @@
 
 # Whilst in the container, run the `arangosh` command and obtain the
 # name of the database
-DBNAME=$(arangosh --server.database $ARANGO_BASE_DB_NAME \
-  --server.username $ARANGO_USER \
-  --server.password $ARANGO_ROOT_PASSWORD \
+DBNAME=$(arangosh --server.database "$ARANGO_BASE_DB_NAME" \
+  --server.username "$ARANGO_USER" \
+  --server.password "$ARANGO_ROOT_PASSWORD" \
   --javascript.execute-string "print(db._name())");
 
 # The exit code is 0 if the database name returned is
