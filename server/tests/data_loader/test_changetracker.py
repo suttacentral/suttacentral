b652c7dd6452bc4769531de8fd71b1fd14c2cb66
jr <jhanarato@gmail.com>
2026-04-20 11:51:30 +0800

    Copy tests over. Two tests failing.

--- server/tests/data_loader/test_changetracker.py
+++ server/tests/data_loader/test_changetracker.py
@@ -1,3 +1,5 @@
+import pytest
+
 import common.utils
 from common import arangodb
 from data_loader.change_tracker import whoami, who_is_calling, function_source, ChangeTracker
@@ -135,3 +137,66 @@ class TestChangeTracker:
             ]
 
             assert list(tracker.changed_files(to_check)) == [tmp_path / '123.txt']
+
+@pytest.fixture
+def mtimes_db():
+    app_ = common.utils.current_app()
+    app_.config['ARANGO_DB'] = 'suttacentral_data_load_tests'
+
+    with app_.app_context():
+        db = arangodb.get_db()
+        db.collection('mtimes').truncate()
+        yield db
+
+class TestIsFileNewOrChanged:
+    def test_true_when_new(self, mtimes_db, tmp_path):
+        tracker = ChangeTracker(base_dir=tmp_path, db=mtimes_db)
+        tracker.update_mtimes()
+        new_file = tmp_path / 'new_file.txt'
+        new_file.touch()
+        tracker = ChangeTracker(base_dir=tmp_path, db=mtimes_db)
+        assert tracker.is_file_new_or_changed(path=new_file)
+
+    def test_true_when_changed(self, mtimes_db, tmp_path):
+        new_file = tmp_path / 'new_file.txt'
+        new_file.touch()
+        tracker = ChangeTracker(base_dir=tmp_path, db=mtimes_db)
+        tracker.update_mtimes()
+        new_file.touch()
+        tracker = ChangeTracker(base_dir=tmp_path, db=mtimes_db)
+        assert tracker.is_file_new_or_changed(path=new_file)
+
+    def test_false_when_not_new_or_changed(self, mtimes_db, tmp_path):
+        new_file = tmp_path / 'new_file.txt'
+        new_file.touch()
+        tracker = ChangeTracker(base_dir=tmp_path, db=mtimes_db)
+        tracker.update_mtimes()
+        tracker = ChangeTracker(base_dir=tmp_path, db=mtimes_db)
+        assert not tracker.is_file_new_or_changed(path=new_file)
+
+
+class TestIsAnyFileNewOrChanged:
+    def test_true_when_one_new(self, mtimes_db, tmp_path):
+        tracker = ChangeTracker(base_dir=tmp_path, db=mtimes_db)
+        tracker.update_mtimes()
+        new_file = tmp_path / 'new_file.txt'
+        new_file.touch()
+        tracker = ChangeTracker(base_dir=tmp_path, db=mtimes_db)
+        assert tracker.is_any_file_new_or_changed(files=[new_file])
+
+    def test_true_when_one_changed(self, mtimes_db, tmp_path):
+        new_file = tmp_path / 'new_file.txt'
+        new_file.touch()
+        tracker = ChangeTracker(base_dir=tmp_path, db=mtimes_db)
+        tracker.update_mtimes()
+        new_file.touch()
+        tracker = ChangeTracker(base_dir=tmp_path, db=mtimes_db)
+        assert tracker.is_any_file_new_or_changed(files=[new_file])
+
+    def test_false_when_none_new_or_changed(self, mtimes_db, tmp_path):
+        new_file = tmp_path / 'new_file.txt'
+        new_file.touch()
+        tracker = ChangeTracker(base_dir=tmp_path, db=mtimes_db)
+        tracker.update_mtimes()
+        tracker = ChangeTracker(base_dir=tmp_path, db=mtimes_db)
+        assert not tracker.is_any_file_new_or_changed(files=[new_file])
