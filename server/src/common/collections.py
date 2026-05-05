7074cc421610df51d2ce26c4b8402fbea716f1d4
jr <jhanarato@gmail.com>
2026-05-05 09:34:53 +0800

    Introduce Collections class
    This is a wrapper around the ArangoDB API relating to collections. The idea is to expose as little of the framework as possible.
    
    Issue #3618

---
+++ server/src/common/collections.py
@@ -0,0 +1,35 @@
+from collections.abc import Sequence
+from typing import Any
+
+from arango.collection import StandardCollection
+from arango.database import StandardDatabase
+
+from common.arangodb import get_db
+from common.utils import current_app
+
+
+def database() -> StandardDatabase:
+    app = current_app()
+    with app.app_context():
+        return get_db()  # type: ignore
+
+
+class Collection:
+    """ SuttaCentral wrapper class for ArangoDB's collection API.
+    Exposes required functionality whilst hiding the rest.
+    """
+    def __init__(self, name: str):
+        self._collection: StandardCollection = database()[name]
+
+    @property
+    def name(self) -> str:
+        return self._collection.name
+
+    def recreate(self, documents: Sequence[dict[str, Any]]) -> None:
+        self._collection.import_bulk_logged(documents, wipe=True)
+
+    def __len__(self) -> int:
+        return len(self._collection)
+
+    def keys(self):
+        return self._collection.keys()
