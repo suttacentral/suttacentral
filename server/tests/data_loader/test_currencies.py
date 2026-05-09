d375c40c9ff405ecbc3e04ec6f14d17385547097
jr <jhanarato@gmail.com>
2026-05-09 09:37:16 +0800

    Add tests for load_currencies()
    Issue #3618

---
+++ server/tests/data_loader/test_currencies.py
@@ -0,0 +1,74 @@
+import json
+from pathlib import Path
+
+import pytest
+
+from common.collections import database, Collection
+from data_loader.currencies import load_currencies
+
+AUD = {
+    "name": "Australian Dollar",
+    "symbol": "AUD",
+    "american_express": True,
+    "use": True,
+    "decimal": True
+}
+
+BOB = {
+    "name": "Bolivian Boliviano",
+    "symbol": "BOB",
+    "american_express": False,
+    "use": True,
+    "decimal": True
+}
+
+COP = {
+    "name": "Colombian Peso",
+    "symbol": "COP",
+    "american_express": False,
+    "use": True,
+    "decimal": True
+}
+
+
+@pytest.fixture
+def one_currency(tmp_path) -> Path:
+    data = [AUD]
+    path = tmp_path / 'currencies.json'
+    with path.open("w") as f:
+        json.dump(data, f)
+
+    return tmp_path
+
+
+@pytest.fixture
+def three_currencies(tmp_path) -> Path:
+    data = [AUD, BOB, COP]
+    path = tmp_path / 'currencies.json'
+    with path.open("w") as f:
+        json.dump(data, f)
+
+    return tmp_path
+
+
+class TestLoadCurrencies:
+    def test_currencies_are_loaded(self, three_currencies):
+        db = database()
+        load_currencies(db, three_currencies)
+        symbols = [doc['symbol'] for doc in Collection('currencies').documents()]
+        assert sorted(symbols) == ['AUD', 'BOB', 'COP']
+
+    def test_currency_names_are_loaded(self, three_currencies):
+        db = database()
+        load_currencies(db, three_currencies)
+        symbols = [doc['symbol'] for doc in Collection('currency_names').documents()]
+        assert sorted(symbols) == ['AUD', 'BOB', 'COP']
+
+    def test_transform_currency_to_currency_name(self, one_currency):
+        db = database()
+        load_currencies(db, one_currency)
+        document = next(Collection('currency_names').documents())
+        assert document['_key'] == 'AUD_en'
+        assert document['name'] == 'Australian Dollar'
+        assert document['lang'] == 'en'
+        assert document['symbol'] == 'AUD'
