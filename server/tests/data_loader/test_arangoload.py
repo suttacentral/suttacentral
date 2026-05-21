63748b1b24acb32494df79262e9ee4cbd7cf96c1
jr <jhanarato@gmail.com>
2026-05-21 13:09:20 +0800

    Add tests for load_json_file()

--- server/tests/data_loader/test_arangoload.py
+++ server/tests/data_loader/test_arangoload.py
@@ -1,11 +1,17 @@
+import json
 from pathlib import Path
+from typing import Generator
 
 import pytest
 
 from common.arangodb import get_db, delete_db
+from common.collections import Collection, database
 from common.utils import current_app
 from data_loader import arangoload
+from data_loader.arangoload import load_json_file
+from fakes import FakeFileChangeTracker
 from data_loader.observability import save_as_csv
+from data_loader.ports import FileChangeTracker
 from migrations.runner import run_migrations
 
 
@@ -45,3 +51,96 @@ def test_do_entire_run(data_load_app):
         printer = arangoload.run(no_pull=False)
         assert len(printer.stages) == 51
         save_as_csv(printer.stages, "load-data-run.csv")
+
+
+class TestLoadJsonFile:
+    @pytest.fixture
+    def with_collection(self):
+        db = database()
+        if not db.has_collection('hobbits'):
+            db.create_collection('hobbits')
+
+    @pytest.fixture
+    def empty_collection(self, with_collection) -> Generator[Collection, None, None]:
+        coll = Collection('hobbits')
+        coll.clear()
+        yield coll
+        coll.clear()
+
+    @pytest.fixture
+    def with_existing_document(self, empty_collection):
+        empty_collection.recreate(
+            [
+                {'_key': 'pippin', 'uid': 'pippin'},
+            ]
+        )
+        return empty_collection
+
+    @pytest.fixture
+    def file_location(self, tmp_path) -> Path:
+        return tmp_path / Path('hobbits.json')
+
+    @pytest.fixture
+    def with_data(self, file_location):
+        data = [
+            {'uid': 'frodo'},
+            {'uid': 'bilbo'}
+        ]
+
+        with file_location.open("w") as f:
+            json.dump(data, f)
+
+    @pytest.fixture
+    def file_is_changed(self, file_location) -> FileChangeTracker:
+        tracker = FakeFileChangeTracker()
+        tracker.change_file(file_location)
+        return tracker
+
+    @pytest.fixture
+    def file_is_not_changed(self, file_location) -> FileChangeTracker:
+        tracker = FakeFileChangeTracker()
+        return tracker
+
+    def test_collection_named_with_file_stem(self, empty_collection, with_data, file_is_changed, file_location):
+        load_json_file(database(), file_is_changed, file_location)
+        assert len(Collection(file_location.stem)) == 2
+
+    def test_uids_are_assigned_to_keys(self, empty_collection, with_data, file_is_changed, file_location):
+        load_json_file(database(), file_is_changed, file_location)
+        assert sorted(list(Collection('hobbits').keys())) == ['bilbo', 'frodo']
+
+    def test_recreates_collection(self, with_existing_document, with_data, file_is_changed, file_location):
+        load_json_file(database(), file_is_changed, file_location)
+        assert sorted(list(Collection('hobbits').keys())) == ['bilbo', 'frodo']
+
+    def test_collection_unchanged_if_file_unchanged(self, with_existing_document, with_data, file_is_not_changed, file_location):
+        load_json_file(database(), file_is_not_changed, file_location)
+        assert sorted(list(Collection('hobbits').keys())) == ['pippin']
+
+    @pytest.fixture
+    def with_first_uid_missing(self, file_location):
+        data = [
+            {'name': 'frodo'},
+            {'uid': 'bilbo'}
+        ]
+
+        with file_location.open("w") as f:
+            json.dump(data, f)
+
+    def test_collection_unchanged_when_first_record_missing_uid(self, with_existing_document, with_first_uid_missing, file_is_changed, file_location):
+        load_json_file(database(), file_is_changed, file_location)
+        assert sorted(list(Collection('hobbits').keys())) == ['pippin']
+
+    @pytest.fixture
+    def with_second_uid_missing(self, file_location):
+        data = [
+            {'uid': 'frodo'},
+            {'name': 'bilbo'}
+        ]
+
+        with file_location.open("w") as f:
+            json.dump(data, f)
+
+    def test_key_error_when_second_record_missing_uid(self, empty_collection, with_second_uid_missing, file_is_changed, file_location):
+        with pytest.raises(KeyError):
+            load_json_file(database(), file_is_changed, file_location)
