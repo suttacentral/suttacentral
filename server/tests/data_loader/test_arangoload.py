4264881d3d2b5bb5362d49fad5182e235595a7c9
jr <jhanarato@gmail.com>
2026-05-22 17:43:16 +0800

    Add tests for process_difficulty()

--- server/tests/data_loader/test_arangoload.py
+++ server/tests/data_loader/test_arangoload.py
@@ -1,10 +1,14 @@
+import json
 from pathlib import Path
+from typing import Generator
 
 import pytest
 
 from common.arangodb import get_db, delete_db
+from common.collections import Collection, database
 from common.utils import current_app
 from data_loader import arangoload
+from data_loader.arangoload import process_difficulty
 from data_loader.observability import save_as_csv
 from migrations.runner import run_migrations
 
@@ -45,3 +49,61 @@ def test_do_entire_run(data_load_app):
         printer = arangoload.run(no_pull=False)
         assert len(printer.stages) == 51
         save_as_csv(printer.stages, "load-data-run.csv")
+
+
+class TestProcessDifficulty:
+    @pytest.fixture
+    def empty_collection(self) -> Generator[Collection, None, None]:
+        coll = Collection('difficulties')
+        coll.clear()
+        yield coll
+        coll.clear()
+
+    @pytest.fixture
+    def with_existing_document(self, empty_collection):
+        empty_collection.recreate(
+            [
+                {
+                    "uid": "dn3",
+                    "difficulty": 2
+                }
+            ]
+        )
+        return empty_collection
+
+    @pytest.fixture
+    def additional_info_dir(self, tmp_path) -> Path:
+        return tmp_path
+
+    @pytest.fixture
+    def file_location(self, additional_info_dir) -> Path:
+        return additional_info_dir / Path('difficulties.json')
+
+    @pytest.fixture
+    def data(self) -> dict:
+        return {
+            "dn": {
+                "dn1": 3,
+            },
+            "mn": {
+                "mn1": 3,
+            },
+        }
+
+    @pytest.fixture
+    def with_data(self, file_location, data):
+        with file_location.open("w") as f:
+            json.dump(data, f)
+
+    def get_difficulties(self) -> list[tuple[str, int]]:
+        difficulties = [(doc['uid'], doc['difficulty']) for doc in Collection('difficulties').documents()]
+        return sorted(difficulties, key=lambda pair: pair[0])
+
+    def test_populates_empty_collection(self, empty_collection, with_data, additional_info_dir):
+        process_difficulty(database(), additional_info_dir)
+        assert self.get_difficulties() == [('dn1', 3), ('mn1', 3)]
+
+    def test_recreates_collection(self, with_existing_document, with_data, additional_info_dir):
+        assert self.get_difficulties() == [('dn3', 2)]
+        process_difficulty(database(), additional_info_dir)
+        assert self.get_difficulties() == [('dn1', 3), ('mn1', 3)]
