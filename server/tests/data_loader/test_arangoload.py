6b2bbe63d920b0bf7be6a5d1b3f8ec886cb9def7
jr <jhanarato@gmail.com>
2026-05-24 10:14:57 +0800

    Add tests for loads_guides_file()

--- server/tests/data_loader/test_arangoload.py
+++ server/tests/data_loader/test_arangoload.py
@@ -1,10 +1,14 @@
+import json
 from pathlib import Path
+from typing import Generator
 
 import pytest
 
 from common.arangodb import get_db, delete_db
+from common.collections import Collection, database
 from common.utils import current_app
 from data_loader import arangoload
+from data_loader.arangoload import load_guides_file
 from data_loader.observability import save_as_csv
 from migrations.runner import run_migrations
 
@@ -45,3 +49,62 @@ def test_do_entire_run(data_load_app):
         printer = arangoload.run(no_pull=False)
         assert len(printer.stages) == 51
         save_as_csv(printer.stages, "load-data-run.csv")
+
+
+class TestLoadGuidesFile:
+    @pytest.fixture
+    def empty_collection(self) -> Generator[Collection, None, None]:
+        coll = Collection('guides')
+        coll.clear()
+        yield coll
+        coll.clear()
+
+    @pytest.fixture
+    def with_existing_document(self, empty_collection):
+        empty_collection.recreate(
+            [
+                {
+                    'guide_uid': 'discourses-guide-sujato',
+                    'text_uid': 'sutta',
+                    'lang': 'en',
+                }
+            ]
+        )
+        return empty_collection
+
+    @pytest.fixture
+    def structure_dir(self, tmp_path) -> Path:
+        return tmp_path
+
+    @pytest.fixture
+    def file_location(self, structure_dir) -> Path:
+        return structure_dir / Path('guides.json')
+
+    @pytest.fixture
+    def data(self) -> list[dict]:
+        return [
+            {
+                'guide_uid': 'vinaya-guide-brahmali',
+                'text_uid': 'vinaya',
+                'lang': 'en'
+            },
+            {
+                'guide_uid': 'abhidhamma-guide-sujato',
+                'text_uid': 'abhidhamma',
+                'lang': 'en'
+            },
+        ]
+
+    @pytest.fixture
+    def with_data(self, file_location, data):
+        with file_location.open("w") as f:
+            json.dump(data, f)
+
+    def test_populates_empty_collection(self, empty_collection, with_data, file_location):
+        load_guides_file(database(), file_location)
+        assert sorted([doc['guide_uid'] for doc in Collection('guides').documents()]) == ['abhidhamma-guide-sujato', 'vinaya-guide-brahmali']
+
+    def test_recreates_collection(self, with_existing_document, with_data, file_location):
+        assert sorted([doc['guide_uid'] for doc in Collection('guides').documents()]) == ['discourses-guide-sujato']
+        load_guides_file(database(), file_location)
+        assert sorted([doc['guide_uid'] for doc in Collection('guides').documents()]) == ['abhidhamma-guide-sujato', 'vinaya-guide-brahmali']
